c824283fc5f58d959f22a61953d3e3c17bc01c41
irakump <ira.kumpula@outlook.com>
2026-01-30 18:53:38 +0200

    Add NavBar to Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import AvailableFilter from '../components/home/FilterAvailable';
 import SearchBar from '../components/home/SearchBar';
 import { SearchResults } from '../components/home/SearchResults';
+import NavBar from '../components/navbar/NavBar';
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       <h1>Home</h1>
       <p>Welcome to the home page!</p>
 
