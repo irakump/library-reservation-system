86a750a146289e84a58f6c9cd2fee45ce1667f7c
irakump <ira.kumpula@outlook.com>
2026-01-30 19:25:57 +0200

    Add mobile layout to NavBar

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div className="bg-navbar">Moi tässä on navbar</div>;
+  return (
+    <div className="flex flex-row justify-between items-center bg-navbar p-2">
+      <p className="font-bold text-3xl">MetBook</p>
+      <div className="flex flex-row gap-3 items-center">
+        <img src="/language-icon.png" alt="Language" className="h-8" />
+        <img src="/hamburger-menu.png" alt="Menu" className="h-10" />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
