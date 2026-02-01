763851c08355ef9db0d982dd6a86027e9800885c
nurhana <nur.hana@metropolia.fi>
2026-02-02 00:40:17 +0200

    Add styling in profile

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,15 +1,20 @@
 import UserInformation from "../components/profile/UserInformation.jsx";
 import Navbar from "../components/navbar/Navbar.jsx";
+import ProfileStats from "../components/profile/ProfileStats.jsx";
 
 const ProfilePage = () => {
   return (
     <>
       <Navbar />
-      <h1 className="text-2xl font-bold text-center text-heading p-4">
-        Hello, user123!
-      </h1>
-      <div className="mx-auto p-4 space-y-4 bg-profileBackground ">
-        <UserInformation />
+      <div className="bg-background min-h-screen">
+        <h1 className="text-2xl font-bold text-center text-heading p-7 ">
+          Hello, user123!
+        </h1>
+        <div className="mx-auto p-4 space-y-4 bg-profileBackground ">
+          <ProfileStats />
+
+          <UserInformation />
+        </div>
       </div>
     </>
   );
