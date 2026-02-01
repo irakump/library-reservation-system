2330cea772abf0dc78325e68654055873d27428b
irakump <ira.kumpula@outlook.com>
2026-02-01 18:26:09 +0200

    Add TODO comment to ProfileMenu

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const ProfileMenu = () => {
+    
+    // TODO: open profile pages by clicking li elements
+    const openPage = () => {
+        console.log('MyPage pressed!');
+    }
+
   return (
     <div>
       <ul className="flex flex-col w-full bg-navbar border border-t-0 *:border-t *:p-2 text-3xl font-bold">
-        <li>My Page</li>
+        <li onClick={openPage}>My Page</li>
         <li>Loans</li>
         <li>Reservations</li>
         <li>History</li>
