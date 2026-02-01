55891806d07781928fcb5643156812222b8cc4db
irakump <ira.kumpula@outlook.com>
2026-02-01 18:04:24 +0200

    Add ProfileMenu to NavBar

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileMenu from "./ProfileMenu";
 
 const NavBar = () => {
+  const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+
   return (
-    <div className="flex flex-row justify-between items-center bg-navbar p-2">
-      <p className="font-bold text-3xl">MetBook</p>
-      <div className="flex flex-row gap-3 items-center">
-        <img src="/language-icon.png" alt="Language" className="h-8" />
-        <img src="/hamburger-menu.png" alt="Menu" className="h-10" />
+    <div>
+      <div className="flex flex-row justify-between items-center bg-navbar p-2">
+        <p className="font-bold text-3xl">MetBook</p>
+        <div className="flex flex-row gap-3 items-center h-10">
+          <img src="/language-icon.png" alt="Language" className="h-8" />
+          {profileMenuOpen ? (
+            <button className="text-6xl font-light pb-3 mr-2 w-8">
+              &times;
+            </button>
+          ) : (
+            <img src="/hamburger-menu.png" alt="Menu" className="h-10" />
+          )}
+        </div>
       </div>
+      {profileMenuOpen ? <ProfileMenu /> : <div></div>}
     </div>
   );
 };
