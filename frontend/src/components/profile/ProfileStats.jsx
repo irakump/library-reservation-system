b23e2036a01e866ce4f058a53659d5efa7bf7211
nurhana <nur.hana@metropolia.fi>
2026-02-02 00:06:47 +0200

    Add profile stats hardcoded data

---
+++ frontend/src/components/profile/ProfileStats.jsx
@@ -0,0 +1,15 @@
+const ProfileStats = () => {
+  const stats = [
+    { id: "loans", label: "Current loans:", value: "4" },
+    {
+      id: "reservations",
+      label: "Reservations:",
+      value: "11",
+    },
+    { id: "favourites", label: "Favourites:", value: "22" },
+  ];
+
+  return <></>;
+};
+
+export default ProfileStats;
