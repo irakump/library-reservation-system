e6b8716f8abd9f8b6bf05103facb087a9a459dff
nurhana <nur.hana@metropolia.fi>
2026-02-01 18:55:50 +0200

    Add styling in userinformation

--- frontend/src/components/profile/UserInformation.jsx
+++ frontend/src/components/profile/UserInformation.jsx
@@ -7,14 +7,20 @@ const UserInformation = () => {
 
   return (
     <>
-      <h1>User Information</h1>
-      <div>
-        {userInfo.map((info) => (
-          <div key={info.id}>
-            <p>{info.label}</p>
-            <p>{info.value}</p>
-          </div>
-        ))}
+      <div className="bg-white rounded-lg p-3 shadow-sm">
+        <h3 className="text-lg font-semibold mb-4 text-left">
+          User Information
+        </h3>
+        <div className="space-y-2">
+          {userInfo.map((info) => (
+            <div key={info.id} className="flex gap-4">
+              <span className="w-32 text-sm font-semibold">{info.label}</span>
+              <span className="text-sm font-medium break-all">
+                {info.value}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
