892c303e8d70593ac419c0a62cdf8452ef66e085
Anni <anni.alanen@metropolia.fi>
2026-02-01 12:30:14 +0200

    added grid for search results and mockdata

---
+++ frontend/src/components/home/BookInfo.jsx
@@ -0,0 +1,11 @@
+const BookInfo = () => {
+    return (
+        <>
+          <div className="bg-white rounded shadow-xl min-h-[50px]">
+
+          </div>
+        </>
+    )
+
+}
+export default BookInfo
--- frontend/src/components/home/SearchResults.jsx
+++ frontend/src/components/home/SearchResults.jsx
@@ -1,9 +1,21 @@
+import BookInfo from './BookInfo';
 import SearchResultsNavigation from './SearchResultsNavigation';
 
 export const SearchResults = () => {
+
+  const books = [{id: 1, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 2,name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 3, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}]  //mockdata
+
   return (
     <>
       <div>SearchResults</div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-x-2 gap-y-3 bg-tag'>
+        {books.map(book => 
+          <BookInfo 
+          key={book.id}
+          book={book}
+          />
+        )}
+      </div>
       <SearchResultsNavigation />
     </>
   );
