f4b5ad1d77fa86df1ce8f87bc4c42d6139582885
nurhana <nur.hana@metropolia.fi>
2026-02-01 17:35:24 +0200

    Add navbar and Userinformation in profilr

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,10 +1,16 @@
+import UserInformation from "../components/profile/UserInformation.jsx";
+import Navbar from "../components/navbar/Navbar.jsx";
+
 const ProfilePage = () => {
   return (
     <>
+      <Navbar />
       <h1 className="text-2xl font-bold text-center text-heading p-4">
         Hello, user123!
       </h1>
-      <div className="mx-auto p-4 space-y-4 bg-profileBackground "></div>
+      <div className="mx-auto p-4 space-y-4 bg-profileBackground ">
+        <UserInformation />
+      </div>
     </>
   );
 };
