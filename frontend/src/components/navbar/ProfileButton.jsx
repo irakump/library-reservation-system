885ab034f694a9395c3ddfec682a85e09d1463bd
irakump <ira.kumpula@outlook.com>
2026-02-01 22:17:57 +0200

    Add MenuContext - Open profile menu on mobile and desktop

--- frontend/src/components/navbar/ProfileButton.jsx
+++ frontend/src/components/navbar/ProfileButton.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useMenu } from "../../contexts/MenuContext";
 
 const ProfileButton = () => {
+  const {toggleMenu} = useMenu();
+
   return (
-    <div className="flex flex-row gap-2 items-center">
-      Profile
-      <img src="dropdown.png" alt="dropdown icon" className="h-7 w-7 mt-1" />
+    <div>
+        <button onClick={toggleMenu}
+         className="flex flex-row gap-2 items-center"
+         >Profile
+            <img src="dropdown.png" alt="dropdown icon" className="h-7 w-7 mt-1" />
+        </button>
+      
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import AvailableFilter from '../components/home/FilterAvailable';
-import SearchBar from '../components/home/SearchBar';
-import { SearchResults } from '../components/home/SearchResults';
-import NavBar from '../components/navbar/NavBar';
+import AvailableFilter from "../components/home/FilterAvailable";
+import SearchBar from "../components/home/SearchBar";
+import { SearchResults } from "../components/home/SearchResults";
+import NavBar from "../components/navbar/NavBar";
+import { MenuProvider } from "../contexts/MenuContext";
 
 const Home = () => {
   return (
     <>
-      <NavBar />
+      <MenuProvider>
+        <NavBar />
+      </MenuProvider>
+
       <h1>Home</h1>
       <p>Welcome to the home page!</p>
 
