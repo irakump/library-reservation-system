b68a0572290accb389896acb3ba5db937d5d61bb
irakump <ira.kumpula@outlook.com>
2026-02-01 19:24:54 +0200

    Remove unused FilterMenu - filtering feature will be in home page, not in navbar

--- frontend/src/components/navbar/DesktopMenu.jsx
+++ frontend/src/components/navbar/DesktopMenu.jsx
@@ -4,7 +4,6 @@ import LoginButton from "./LoginButton";
 const DesktopMenu = () => {
   return (
     <div className="flex flex-row gap-10 pl-5 pr-10 font-bold text-2xl">
-      <div>Browse</div>
       <div>Profile</div>
       <LoginButton />
     </div>
