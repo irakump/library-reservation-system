d07db5a3f81ef2c35a7063fd28da882c9caebbb1
irakump <ira.kumpula@outlook.com>
2026-02-01 22:36:14 +0200

    Add responsive layot to ProfileMenu - Make profile menu smaller, open to the side

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -7,9 +7,11 @@ const ProfileMenu = () => {
         console.log('MyPage pressed!');
     }
 
+    // TODO: set profile menu fixed under profile button
+
   return (
     <div className="flex sm:justify-end">
-      <ul className="flex flex-col w-full sm:w-60 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
+      <ul className="flex flex-col w-full sm:w-55 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
         <li onClick={openPage}>My Page</li>
         <li>Loans</li>
         <li>Reservations</li>
