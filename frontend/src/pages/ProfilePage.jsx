748ca70fbcc4799386e77ea09dd103cdab374c7e
nurhana <nur.hana@metropolia.fi>
2026-02-01 17:12:16 +0200

    Add css styling in header

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,10 @@
 const ProfilePage = () => {
   return (
     <>
-      <h1>Hello, user123!</h1>
+      <h1 className="text-2xl font-bold text-center text-heading p-4">
+        Hello, user123!
+      </h1>
+      <div className="mx-auto p-4 space-y-4 bg-profileBackground "></div>
     </>
   );
 };
