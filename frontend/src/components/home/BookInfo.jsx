a786b5dd55e2c560a73f545d36356f8ca3929332
Anni <anni.alanen@metropolia.fi>
2026-02-01 16:05:33 +0200

    added layout for results

--- frontend/src/components/home/BookInfo.jsx
+++ frontend/src/components/home/BookInfo.jsx
@@ -1,8 +1,21 @@
-const BookInfo = () => {
+const BookInfo = ({book}) => {
     return (
         <>
-          <div className="bg-white rounded shadow-xl min-h-[50px]">
-
+          <div className="bg-white rounded shadow-xl grid grid-cols-3 grid-rows-6 max-h-45 gap-1 ">
+            <div>
+                <img src="https://placehold.co/150x200/png" alt="placeholder" className="m-5 min-w-26 min-h-35 col-end-2"/>
+            </div>
+            <div className="capitalize m-5 col-start-2 row-start-1 mr-auto">
+                <h3 className="font-bold text-xl" >{book.name}</h3>
+                <p>{book.author}</p>
+                <p>{book.year}</p>
+                
+            </div>
+            <div className="col-end-7" >O</div>
+            <span className="col-start-2 row-start-5 m-5">Available</span>
+            <button className=" rounded-md bg-filter ml-auto col-start-7 row-start-5 m-3 px-5">Loan</button>
+            
+            
           </div>
         </>
     )
--- frontend/src/components/home/SearchResults.jsx
+++ frontend/src/components/home/SearchResults.jsx
@@ -3,12 +3,12 @@ import SearchResultsNavigation from './SearchResultsNavigation';
 
 export const SearchResults = () => {
 
-  const books = [{id: 1, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 2,name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 3, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}]  //mockdata
+  const books = [{id: 1, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 2,name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 3, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}, {id: 4, name: "nimi", author: "author", genre: "genre", year: "year", availibility: "available"}]  //mockdata
 
   return (
     <>
       <div>SearchResults</div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 gap-x-2 gap-y-3 bg-tag'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 md:mx-15 lg:mx-15 xl:mx-40 md:grid-rows-7 bg-tag p-15 gap-4'>
         {books.map(book => 
           <BookInfo 
           key={book.id}
