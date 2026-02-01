8ee6e2046e2e6c4ad0685fde04c8b91b6f6f963d
nurhana <nur.hana@metropolia.fi>
2026-02-01 14:50:08 +0200

    Add UserInformation component

---
+++ frontend/src/components/profile/UserInfoCard.jsx
@@ -0,0 +1,12 @@
+const UserInfoCard = () => {
+  return (
+    <>
+      <h1>User Information</h1>
+      <p>Nickname: User123</p>
+      <p>Email: user123@metropolia.fi</p>
+      <p>User Created: 1.1.2024</p>
+    </>
+  );
+};
+
+export default UserInfoCard;
