d07624a38e91d7e080d7dc7a1acbf9e4b9482a32
irakump <ira.kumpula@outlook.com>
2026-02-02 19:45:47 +0200

    Add links to profile pages - Other pages than My Page are not implemented

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProfileMenu = () => {
 
-    // TODO: open profile pages by clicking li elements
+    // TODO: change profile page routes (all components are not separate pages)
     const openPage = () => {
         console.log('MyPage pressed!');
     }
@@ -12,11 +12,11 @@ const ProfileMenu = () => {
   return (
     <div className="flex sm:justify-end">
       <ul className="flex flex-col w-full sm:w-55 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
-        <li onClick={openPage}>My Page</li>
-        <li>Loans</li>
-        <li>Reservations</li>
-        <li>History</li>
-        <li>Favorites</li>
+        <li onClick={openPage}><a href="/profile">My Page</a></li>
+        <li><a href="/profile/loans">Loans</a></li>
+        <li><a href="/profile/reservations">Reservations</a></li>
+        <li><a href="/profile/history">History</a></li>
+        <li><a href="/profile/favorites">Favorites</a></li>
         <li>Log Out →</li>
       </ul>
     </div>
