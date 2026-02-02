1bc1b9218ac59d360ef1f4bc9cc049d1ce11f38b
irakump <ira.kumpula@outlook.com>
2026-02-02 20:59:01 +0200

    Center text in profile menu

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -5,7 +5,7 @@ const ProfileMenu = () => {
     // TODO: change profile page routes (all components are not separate pages, check App.jsx)
 
   return (
-    <div className="absolute max-sm:w-full sm:right-32">
+    <div className="absolute max-sm:w-full sm:right-32 text-center">
       <ul className="flex flex-col w-full sm:w-50 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
         <li><a href="/profile">My Page</a></li>
         <li><a href="/profile/loans">Loans</a></li>
