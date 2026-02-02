68f4ee5d5e63700eecdfcd3b2c0ca593da4799bd
nurhana <nur.hana@metropolia.fi>
2026-02-02 23:01:10 +0200

    Add hardcoded data for favourite page

--- frontend/src/pages/FavouritePage.jsx
+++ frontend/src/pages/FavouritePage.jsx
@@ -1,6 +1,39 @@
 import BookDataPage from "./BookDataPage";
-const FavouritesPage = () => {
-  return <BookDataPage title="My Favourites (4)" />;
+
+const FavouritePage = () => {
+  const favouriteData = [
+    {
+      id: 1,
+      title: "Book Title",
+      author: "Author name",
+      year: "2025",
+    },
+    {
+      id: 1,
+      title: "Book Title",
+      author: "Author name",
+      year: "2025",
+    },
+    {
+      id: 1,
+      title: "Book Title",
+      author: "Author name",
+      year: "2025",
+    },
+    {
+      id: 1,
+      title: "Book Title",
+      author: "Author name",
+      year: "2025",
+    },
+  ];
+  return (
+    <BookDataPage
+      title="My Favourites (4)"
+      books={favouriteData}
+      pageType="favourite"
+    />
+  );
 };
 
-export default FavouritesPage;
+export default FavouritePage;
