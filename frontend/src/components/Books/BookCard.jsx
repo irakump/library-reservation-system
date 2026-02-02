724ca62773e3d819ec2fc34d455b4877912feec1
nurhana <nur.hana@metropolia.fi>
2026-02-02 22:23:32 +0200

    Add page type condition data for reservation

--- frontend/src/components/Books/BookCard.jsx
+++ frontend/src/components/Books/BookCard.jsx
@@ -1,7 +1,7 @@
 const BookCard = ({ book, pageType }) => {
   return (
     <div className="bg-white rounded-lg shadow-sm p-2 flex gap-6">
-      <div className="w-24 h-32 mt-2 bg-gray-300 rounded flex-shrink-0 "></div>
+      <div className="w-24 h-32 mt-1.5 ml-1.5 bg-gray-300 rounded flex-shrink-0 "></div>
 
       <div className="flex-1">
         <div className="flex justify-between items-start">
@@ -18,6 +18,15 @@ const BookCard = ({ book, pageType }) => {
 
             <button className="bg-filter font-semibold rounded-lg px-4 py-2 float-right">
               Return
+            </button>
+          </>
+        )}
+        {pageType === "reservation" && (
+          <>
+            <p className="text-sm mb-1 text-left">🟢 Available</p>
+
+            <button className="bg-filter font-semibold rounded-lg px-4 py-2 float-right">
+              Cancel
             </button>
           </>
         )}
