733719d5f57f02b4fdfb05e868b5c266c62010cb
nurhana <nur.hana@metropolia.fi>
2026-02-02 18:38:39 +0200

    Add hardcoded loans data

--- frontend/src/pages/LoansPage.jsx
+++ frontend/src/pages/LoansPage.jsx
@@ -1,6 +1,18 @@
 import BookDataPage from "./BookDataPage";
 const LoansPage = () => {
-  return <BookDataPage title="My loans (4)" />;
+  const loansData = [
+    {
+      id: 1,
+      title: "Book Title",
+      author: "Author name",
+      year: "2025",
+      dueDate: "1.1.2025",
+    },
+  ];
+
+  return (
+    <BookDataPage title="My loans (4)" books={loansData} pageType="loans" />
+  );
 };
 
 export default LoansPage;
