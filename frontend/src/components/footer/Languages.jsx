6276fcd99f4e1e6d76f54a4776a1664f5578aada
irakump <ira.kumpula@outlook.com>
2026-02-02 21:36:21 +0200

    Add footer draft

---
+++ frontend/src/components/footer/Languages.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Languages = () => {
+  return (
+    <div className="flex flex-col">
+      <h2 className="text-2xl py-5">Language</h2>
+      <div className="flex flex-col gap-2 items-start">
+        <button>Finnish</button>
+        <button>English</button>
+      </div>
+    </div>
+  );
+};
+
+export default Languages;
---
+++ frontend/src/components/footer/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='bg-card mt-5 text-center w-60 h-60'>
+      <a href="/" className=' '>Logo</a>
+    </div>
+  )
+}
+
+export default Logo;
