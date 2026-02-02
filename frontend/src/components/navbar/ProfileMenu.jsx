d268e3d681f2542e2e603275d908de8645b42cf9
irakump <ira.kumpula@outlook.com>
2026-02-02 20:44:20 +0200

    Cleanup: remove console log and todo comment

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -3,16 +3,11 @@ import React from "react";
 const ProfileMenu = () => {
 
     // TODO: change profile page routes (all components are not separate pages, check App.jsx)
-    const openPage = () => {
-        console.log('MyPage pressed!');
-    }
-
-    // TODO: set profile menu fixed under profile button
 
   return (
     <div className="absolute max-sm:w-full sm:right-32">
       <ul className="flex flex-col w-full sm:w-50 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
-        <li onClick={openPage}><a href="/profile">My Page</a></li>
+        <li><a href="/profile">My Page</a></li>
         <li><a href="/profile/loans">Loans</a></li>
         <li><a href="/profile/reservations">Reservations</a></li>
         <li><a href="/profile/history">History</a></li>
