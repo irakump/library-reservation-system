4822bdf537bff546df6e9c3ba739feb91232acdb
nurhana <nur.hana@metropolia.fi>
2026-02-02 22:53:48 +0200

    Add route to reservation, history and favourite page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Home from "./pages/Home.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import LoansPage from "./pages/LoansPage.jsx";
+import ReservationPage from "./pages/ReservationPage.jsx";
+import HistoryPage from "./pages/HistoryPage.jsx";
+import FavouritePage from "./pages/FavouritePage.jsx";
 
 import { Routes, Route } from "react-router";
 
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/loans" element={<LoansPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/favourite" element={<FavouritePage />} />
+        <Route path="/reservation" element={<ReservationPage />} />
       </Routes>
     </>
   );
