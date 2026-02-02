754187ac6bb816883ba0d072c53d85dad88a068e
nurhana <nur.hana@metropolia.fi>
2026-02-02 20:09:31 +0200

    Add styling in book data page

--- frontend/src/pages/BookDataPage.jsx
+++ frontend/src/pages/BookDataPage.jsx
@@ -1,13 +1,23 @@
 import NavBar from "../components/navbar/NavBar.jsx";
 import { MenuProvider } from "../contexts/MenuContext.jsx";
+import BookCard from "../components/books/BookCard.jsx";
 
-const BookDataPage = ({ title }) => {
+const BookDataPage = ({ title, books, pageType }) => {
   return (
     <>
       <MenuProvider>
         <NavBar />
       </MenuProvider>
-      <h1>{title}</h1>
+      <div className="bg-background min-h-screen">
+        <h1 className="text-2xl font-bold text-center text-heading p-7">
+          {title}
+        </h1>
+        <div className="mx-auto p-4 space-y-4 bg-profileBackground">
+          {books.map((book) => (
+            <BookCard key={book.id} book={book} pageType={pageType} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
