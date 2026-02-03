1f5f7990f792e6e337162a970267710e6658e440
nurhana <nur.hana@metropolia.fi>
2026-02-03 19:38:21 +0200

    Modified styling for better alignment with footer and support desktop compatible

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -7,18 +7,20 @@ import Footer from "../components/footer/Footer";
 const ProfilePage = () => {
   return (
     <>
-      <MenuProvider>
-        <NavBar />
-      </MenuProvider>
-      <div className="bg-background min-h-screen">
-        <h1 className="text-2xl font-bold text-center text-heading p-7 ">
-          Hello, user123!
-        </h1>
-        <div className="mx-auto p-4 space-y-4 bg-profileBackground rounded-sm">
-          <ProfileStats />
+      <div className="min-h-screen flex flex-col">
+        <MenuProvider>
+          <NavBar />
+        </MenuProvider>
+        <main className="flex-1 bg-background pb-12">
+          <h1 className="text-2xl font-bold text-center text-heading p-7 ">
+            Hello, user123!
+          </h1>
+          <div className="mx-auto p-4 max-w-2xl space-y-4 bg-profileBackground rounded-sm">
+            <ProfileStats />
 
-          <UserInformation />
-        </div>
+            <UserInformation />
+          </div>
+        </main>
         <Footer />
       </div>
     </>
