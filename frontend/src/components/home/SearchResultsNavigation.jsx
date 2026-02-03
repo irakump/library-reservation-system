eca0bc93e657fd08ad61bdfa6f1088fa3fc8d426
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-03 03:05:28 +0200

    add search results navigation

--- frontend/src/components/home/SearchResultsNavigation.jsx
+++ frontend/src/components/home/SearchResultsNavigation.jsx
@@ -1,5 +1,20 @@
 const SearchResultsNavigation = () => {
-  return <div>SearchResultNavigation</div>;
+  return (
+    <>
+      <div className="flex flex-row items-center">
+        <button className="button rounded-md">{'<'}</button>
+
+        {/* Links to search result pages */}
+        <div className="mx-4 [&>a]:mx-1">
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+        </div>
+
+        <button className="button rounded-md">{'>'}</button>
+      </div>
+    </>
+  );
 };
 
 export default SearchResultsNavigation;
