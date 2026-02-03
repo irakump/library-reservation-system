a8d740b18ce2695dc578e06e6a4e71d4dae113a2
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-03 03:05:13 +0200

    add placeholder search results

--- frontend/src/components/home/SearchResults.jsx
+++ frontend/src/components/home/SearchResults.jsx
@@ -1,10 +1,29 @@
 import SearchResultsNavigation from './SearchResultsNavigation';
 
-export const SearchResults = () => {
+const SearchResults = () => {
   return (
     <>
-      <div>SearchResults</div>
-      <SearchResultsNavigation />
+      {/* Search result header */}
+      <div className="flex flex-row justify-between mb-2">
+        <h2>Search Results</h2>
+        <h2>1 - 20 / 53</h2>
+      </div>
+
+      <div className="bg-blue-200">
+        {/* TODO: replace with actual search results component */}
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, at.
+          Libero, quis accusantium unde molestias nobis corrupti minus error,
+          dignissimos, recusandae esse et neque earum eum quam! Sunt, amet unde?
+        </p>
+      </div>
+
+      {/* Search result navigation */}
+      <div className="flex justify-center mt-2">
+        <SearchResultsNavigation />
+      </div>
     </>
   );
 };
+
+export default SearchResults;
