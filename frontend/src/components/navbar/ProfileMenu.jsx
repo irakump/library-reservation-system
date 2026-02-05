f0ed58ab9e41a591557abf9492fdd5d77aceefed
irakump <ira.kumpula@outlook.com>
2026-02-05 16:26:45 +0200

    Fix App.jsx route order to match profile menu

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 const ProfileMenu = () => {
 
-    // TODO: change profile page routes (all components are not separate pages, check App.jsx)
-
   return (
     <div className="absolute max-sm:w-full sm:right-32 text-center">
       <ul className="flex flex-col w-full sm:w-50 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
