6bf57e27f908e46cd637a66101d925fdeedca2b9
irakump <ira.kumpula@outlook.com>
2026-02-05 16:11:20 +0200

    Finish responsive footer - Placeholder icon needs to be replaced

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -7,12 +7,10 @@ const Footer = () => {
   return (
     <div className="flex flex-col bg-navbar p-5 sm:py-10 items-center">
       <div className="flex flex-col sm:flex-row sm:gap-15 *:md:gap-15 lg:gap-50 *:lg:gap-50">
-        
         <div className="flex flex-col md:flex-row">
           <SocialMedia />
           <Languages />
         </div>
-
         <Logo />
       </div>
     </div>
--- frontend/src/components/footer/Languages.jsx
+++ frontend/src/components/footer/Languages.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Languages = () => {
   return (
   <div className="flex flex-col">
-      <h2 className="text-2xl py-5 sm:pt-0">Language</h2>
+      <h2 className="text-2xl py-5 md:pt-0">Language</h2>
       <div className="flex flex-col gap-2 items-start">
         <button>Finnish</button>
         <button>English</button>
