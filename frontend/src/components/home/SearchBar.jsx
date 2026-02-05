67d792ce2f10dc03f8a9000f328ab11d97f900e9
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-05 23:57:52 +0200

    add filtering to search bar, modify to use form, add mock data

--- frontend/src/components/home/SearchBar.jsx
+++ frontend/src/components/home/SearchBar.jsx
@@ -1,22 +1,60 @@
+import { useState } from 'react';
+
 import MagnifyingGlass from '@heroicons/react/16/solid/MagnifyingGlassIcon';
 
 const SearchBar = () => {
+  const allBooks = ['book1', 'the book2', 'book3', 'harry potter', 'the hobbit'];
+  const [books, setBooks] = useState(allBooks);
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    if (searchTerm.trim() === '') {
+      setBooks(allBooks);
+      return;
+    }
+
+    const filteredBooks = allBooks.filter((book) =>
+      book.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setBooks(filteredBooks);
+  };
+
   return (
     <div className="flex flex-col w-full h-15 gap-1 items-stretch mt-8 mb-12">
       <label className="ml-1" htmlFor="search">
         Search
       </label>
 
-      <div className="flex flex-row flex-1 w-full h-full rounded-sm border border-black divide-x-2 divide-black">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch();
+        }}
+        className="flex flex-row flex-1 w-full h-full rounded-sm border border-black divide-x-2 divide-black"
+      >
         <input
-          className="flex-1 h-full bg-white px-1"
           type="text"
           id="search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="flex-1 h-full bg-white px-1"
         />
-        <div className="flex flex-row h-full px-4 bg-filter gap-1">
+
+        <button
+          type="submit"
+          onClick={handleSearch}
+          className="flex flex-row h-full px-4 bg-filter gap-1 cursor-pointer"
+        >
           <MagnifyingGlass className="h-full size-4" />
-          <button>Search</button>
-        </div>
+          Search
+        </button>
+      </form>
+
+      {/* mock data, delete */}
+      <div>
+        {books.map((book) => (
+          <div key={book}>{book}</div>
+        ))}
       </div>
     </div>
   );
