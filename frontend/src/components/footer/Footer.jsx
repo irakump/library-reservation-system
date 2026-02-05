802a87d02357b0c3a71c4a9f00e4e3d79815ea8d
irakump <ira.kumpula@outlook.com>
2026-02-05 15:49:23 +0200

    Make footer responsive

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import SocialMedia from './SocialMedia';
-import Languages from './Languages';
-import Logo from './Logo';
+import React from "react";
+import SocialMedia from "./SocialMedia";
+import Languages from "./Languages";
+import Logo from "./Logo";
 
 const Footer = () => {
   return (
-    <div className='bg-navbar p-5 block'>
-      <SocialMedia />
-      <Languages />
-      <Logo />
+    <div className="flex flex-col bg-navbar p-5 sm:py-10 items-center">
+      <div className="flex flex-col sm:flex-row sm:gap-15 *:md:gap-15 lg:gap-50 *:lg:gap-50">
+        
+        <div className="flex flex-col md:flex-row">
+          <SocialMedia />
+          <Languages />
+        </div>
+
+        <Logo />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
--- frontend/src/components/footer/Logo.jsx
+++ frontend/src/components/footer/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='bg-card mt-5 text-center w-60 h-60'>
+    <div className='bg-card mt-5 sm:mt-0 text-center w-60 h-60'>
       <a href="/" className=' '>Logo</a>
     </div>
   )
