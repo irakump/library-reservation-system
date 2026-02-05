eadb4c67a6ac88f7d0c78512ad95d739d50229a3
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-05 23:48:42 +0200

    add cursor pointer to filter tags when hovered

--- frontend/src/components/home/FilterTag.jsx
+++ frontend/src/components/home/FilterTag.jsx
@@ -4,7 +4,7 @@ const FilterTag = ({ filterName, onRemove, closeButton = true }) => {
   return (
     <button
       onClick={onRemove}
-      className="text-xs flex items-center rounded-md p-1 bg-tag border border-tagBorder"
+      className="text-xs flex items-center rounded-md p-1 bg-tag border border-tagBorder cursor-pointer"
     >
       <span className="flex-1">{filterName}</span>
       {closeButton && <XMark className="ml-1 size-4" />}
