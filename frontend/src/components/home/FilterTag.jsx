be27c36f5b76a009e32fdc888caefe952d60acb5
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-06 01:45:08 +0200

    add margin to filter tag

--- frontend/src/components/home/FilterTag.jsx
+++ frontend/src/components/home/FilterTag.jsx
@@ -6,7 +6,7 @@ const FilterTag = ({ filterName, onRemove, closeButton = true }) => {
       onClick={onRemove}
       className="text-xs flex items-center rounded-md p-1 bg-tag border border-tagBorder cursor-pointer"
     >
-      <span className="flex-1">{filterName}</span>
+      <span className="flex-1 ml-1">{filterName}</span>
       {closeButton && <XMark className="ml-1 size-4" />}
     </button>
   );
