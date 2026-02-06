cfc62bc9b08eeca76b0eb952ba09062b543d0b21
irakump <ira.kumpula@outlook.com>
2026-02-06 12:13:30 +0200

    Make profile menu rows fully clickable - Wrap li elements with links

--- frontend/src/components/navbar/ProfileMenu.jsx
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 
 const ProfileMenu = () => {
-
   return (
     <div className="absolute max-sm:w-full sm:right-32 text-center">
       <ul className="flex flex-col w-full sm:w-50 bg-navbar border border-t-0 *:border-t *:p-2 text-2xl font-bold">
-        <li><a href="/profile">My Page</a></li>
-        <li><a href="/profile/loans">Loans</a></li>
-        <li><a href="/profile/reservations">Reservations</a></li>
-        <li><a href="/profile/history">History</a></li>
-        <li><a href="/profile/favorites">Favorites</a></li>
-        <li className="sm:hidden"><button>Log out →</button></li>
+        <a href="/profile" className="w-full">
+          <li>My Page</li>
+        </a>
+        
+        <a href="/profile/loans" className="w-full">
+          <li>Loans</li>
+        </a>
+
+        <a href="/profile/reservations" className="w-full">
+          <li>Reservations</li>
+        </a>
+
+        <a href="/profile/history" className="w-full">
+          <li>History</li>
+        </a>
+
+        <a href="/profile/favorites" className="w-full">
+          <li>Favorites</li>
+        </a>
+
+        <a href="/logout" className="w-full sm:hidden">
+          <li>Log out →</li>
+        </a>
       </ul>
     </div>
   );
