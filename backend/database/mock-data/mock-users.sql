30cb55c7898bf99ff22fa3ad6cfb8fdadb15ec24
irakump <ira.kumpula@outlook.com>
2026-02-06 17:21:30 +0200

    Add mock users

---
+++ backend/database/mock-data/mock-users.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS (Email, Nickname, PasswordHash, CreatedAt)
+VALUES ('dumbledore@hogwarts.com', 'Dumbledore', 'tosihyvähashattusalasan4j33j33M01!!', '2018-01-09T14:23:51'),
+('gandalf@youshallnotpass.com', 'Gandalf', '128309a02k5hgjh648AGDw675jJgVB', '2026-02-06T17:00:01');
