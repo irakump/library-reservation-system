79cdae6524cf1310307974d30447763c82f8a240
irakump <ira.kumpula@outlook.com>
2026-02-06 11:59:28 +0200

    Add logos to footer, navbar and favicon

--- frontend/src/components/footer/Logo.jsx
+++ frontend/src/components/footer/Logo.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Logo = () => {
   return (
-    <div className='bg-card mt-5 sm:mt-0 text-center w-60 h-60'>
-      <a href="/" className=' '>Logo</a>
+    <div className="mt-5 sm:mt-0 text-center w-55">
+      <a href="/" className=" ">
+        <img src="/logo.png" alt="MetBook logo" />
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default Logo;
--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,34 +1,48 @@
 import ProfileMenu from "./ProfileMenu";
 import DesktopMenu from "./DesktopMenu";
-import {useMenu} from "../../contexts/MenuContext";
+import { useMenu } from "../../contexts/MenuContext";
 
 const NavBar = () => {
-
-  const {isProfileMenuOpen, toggleMenu} = useMenu();
+  const { isProfileMenuOpen, toggleMenu } = useMenu();
 
   //const toggleMenu = () => setIsProfileMenuOpen(!isProfileMenuOpen);
 
   return (
     <div>
       <div className="flex flex-row justify-between items-center bg-navbar p-2">
-        <a href="/" className="font-bold text-3xl">MetBook</a>
+        <div className="flex flex-row items-center">
+          <a href="/">
+            <img src="/book-logo.png" alt="Stack of books" className="h-7 pr-2 pt-1" />
+          </a>
+          <a href="/" className="font-bold text-3xl">
+            MetBook
+          </a>
+        </div>
         <div className="flex flex-row gap-3 items-center h-10">
-          <img src="/language-icon.png" alt="Language" className="h-8" tabIndex={0} />
+          <img
+            src="/language-icon.png"
+            alt="Language"
+            className="h-8"
+            tabIndex={0}
+          />
 
           <div className="max-sm:hidden">
             <DesktopMenu />
           </div>
 
           <div className="sm:hidden">
-          {isProfileMenuOpen ? (
-            <button onClick={toggleMenu} 
-            className="text-6xl font-light pb-3 mr-2 w-8">
-              &times;
-            </button>
-          ) : (
-            <button className="h-10 mt-2" onClick={toggleMenu}><img src="/hamburger-menu.png" alt="Menu" className="h-10" 
-            /></button>
-          )}
+            {isProfileMenuOpen ? (
+              <button
+                onClick={toggleMenu}
+                className="text-6xl font-light pb-3 mr-2 w-8"
+              >
+                &times;
+              </button>
+            ) : (
+              <button className="h-10 mt-2" onClick={toggleMenu}>
+                <img src="/hamburger-menu.png" alt="Menu" className="h-10" />
+              </button>
+            )}
           </div>
         </div>
       </div>
