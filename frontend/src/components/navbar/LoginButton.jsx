0adf011c2ace4d669d07a97ca99e2ea0f6aef6c6
nurhana <nur.hana@metropolia.fi>
2026-02-09 14:07:13 +0200

    Add functionality to open register and login modal

--- frontend/src/components/navbar/LoginButton.jsx
+++ frontend/src/components/navbar/LoginButton.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React from "react";
+
+import { useState } from "react";
+import LoginModal from "../auth/LoginModal.jsx";
+import RegisterModal from "../auth/RegisterModal.jsx";
 
 const LoginButton = () => {
-    // TODO: Show "log in" if user is not logged in
-  
-    return (
-    <div>
-      <button className='w-30'>Log out</button>
-    </div>
-  )
-}
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // TODO: Replace with actual authentication state
 
-export default LoginButton
+  const handleSwitchToRegister = () => {
+    setIsLoginOpen(false);
+    setIsRegisterOpen(true);
+  };
+
+  const handleSwitchToLogin = () => {
+    setIsRegisterOpen(false);
+    setIsLoginOpen(true);
+  };
+
+  return (
+    <>
+      <div>
+        {isLoggedIn ? (
+          <button className="w-30">Log out</button>
+        ) : (
+          <button className="w-30" onClick={() => setIsLoginOpen(true)}>
+            Login
+          </button>
+        )}
+      </div>
+      <LoginModal
+        isOpen={isLoginOpen}
+        onClose={() => setIsLoginOpen(false)}
+        onSwitchToRegister={handleSwitchToRegister}
+      ></LoginModal>
+
+      <RegisterModal
+        isOpen={isRegisterOpen}
+        onClose={() => setIsRegisterOpen(false)}
+        onSwitchToLogin={handleSwitchToLogin}
+      ></RegisterModal>
+    </>
+  );
+};
+
+export default LoginButton;
