e6068a635d926264c12d2240a918d42f9e4afd05
nurhana <nur.hana@metropolia.fi>
2026-02-09 23:05:56 +0200

    Modified styling and add x button to close modal

--- frontend/src/components/auth/LoginModal.jsx
+++ frontend/src/components/auth/LoginModal.jsx
@@ -3,7 +3,7 @@ const LoginModal = ({ isOpen, onClose, onSwitchToRegister }) => {
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black/40 z-50"
+      className="fixed inset-0  bg-black/40 flex items-center justify-center z-50"
       onClick={onClose}
     >
       <div
@@ -11,35 +11,42 @@ const LoginModal = ({ isOpen, onClose, onSwitchToRegister }) => {
         onClick={(e) => e.stopPropagation()}
       >
         {/* Header */}
-        <div className="bg-navbar p-6">
+        <div className="bg-navbar p-4 flex justify-between text-center">
           <h2 className="text-3xl font-bold text-gray-800">Login</h2>
+
+          <button
+            onClick={onClose}
+            className="text-2xl font-bold text-gray-800 hover:text-black"
+            aria-label="Close modal"
+          >
+            ✕
+          </button>
         </div>
 
         {/* Form */}
-        <div className="p-8"></div>
-        {/* Email */}
-        <div className="mb-6">
-          <label className="block text-gray-600 mb-2 text-sm">Email</label>
-          <input
-            type="email"
-            placeholder="example@gmail.com"
-            className="w-full py-3 px-4 font-medium text-lg border-2 border-gray-300 
+        <div className="p-8">
+          {/* Email */}
+          <div className="mb-6 mt-4">
+            <label className="block text-gray-600 mb-2 text-sm">Email</label>
+            <input
+              type="email"
+              placeholder="example@gmail.com"
+              className="w-full px-4 py-3 font-medium text-lg border-2 border-gray-300 
              rounded-xl focus:outline-none focus:border-blue-400"
-          />
-        </div>
+            />
+          </div>
 
-        {/* Password */}
-        <div className="mb-6">
-          <label className="block text-gray-600 mb-2 text-sm">Password</label>
-          <input
-            type="password"
-            placeholder="Enter your Password"
-            className="w-full py-3 px-4 font-medium text-lg border-2 border-gray-300 
+          {/* Password */}
+          <div className="mb-12">
+            <label className="block text-gray-600 mb-2 text-sm">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              className="w-full px-4 py-3 font-medium text-lg border-2 border-gray-300 
              rounded-xl focus:outline-none focus:border-blue-400"
-          />
+            />
+          </div>
         </div>
-
-        {/**/}
       </div>
     </div>
   );
