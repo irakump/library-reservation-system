008a129111fd6369c60160e9457cbc24f3be8814
nurhana <nur.hana@metropolia.fi>
2026-02-10 00:45:13 +0200

    Add content and style in logout modal

--- frontend/src/components/auth/LogoutModal.jsx
+++ frontend/src/components/auth/LogoutModal.jsx
@@ -1,21 +1,43 @@
 const LogoutModal = ({ isOpen, onClose, onConfirm }) => {
-  if (!open) return null;
+  if (!isOpen) return null;
   return (
     <div
       className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
       onClick={onClose}
     >
       <div
-        className="bg-blue-200 rounded-lg p-6 max-w-sm w-full mx-4"
+        className="bg-tag rounded-lg p-6 max-w-sm w-full mx-4"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex justify-end mb-2">
           <button
             onClick={onClose}
-            className="text-2xl font-bold hover:text-gray-600"
+            className="text-2xl font-bold text-gray-800 hover:text-black"
           >
             ✕
           </button>
+        </div>
+
+        <div className="bg-white rounded-lg p-4">
+          <h2 className="text-xl font-bold text-center mb-6">
+            Are you sure you
+            <br /> want to log out?
+          </h2>
+
+          <div className="flex justify-center gap-8">
+            <button
+              onClick={onClose}
+              className="bg-filter hover:bg-blue-300 font-medium text-lg px-6 py-2 rounded-lg"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={onConfirm}
+              className="bg-filter hover:bg-blue-300 font-medium text-lg px-6 py-2 rounded-lg"
+            >
+              Log out
+            </button>
+          </div>
         </div>
       </div>
     </div>
