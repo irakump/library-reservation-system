f04fd251369a6e04b19b22601a66bb2b5c4d723b
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-10 14:59:57 +0200

    add SearchFilterProvider wrapper to Home components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,9 @@ import SearchFiltersHider from '../components/home/SearchFiltersHider';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { SearchResultProvider } from '../contexts/SearchResultContext';
-
+import { SearchFilterProvider } from '../contexts/SearchFilterContext';
 
 const Home = () => {
-
   // example, delete
   const [genres, setGenres] = useState([]);
   const [users, setUsers] = useState([]);
@@ -18,31 +17,31 @@ const Home = () => {
       .get('http://localhost:8081/api/genre')
       .then((response) => setGenres(response.data))
       .catch((error) => console.error(error));
-  
-    }, []);
+  }, []);
 
   console.log('Genres:', genres);
 
-
-    useEffect(() => {
-      axios
+  useEffect(() => {
+    axios
       .get('http://localhost:8081/api/users')
       .then((response) => setUsers(response.data))
       .catch((error) => console.error(error));
-    }, []);
+  }, []);
 
   console.log('Users: ', users);
   //
 
   return (
     <>
-      <SearchResultProvider>
-        <div className="p-4 bg-background">
-          <SearchBar />
-          <SearchFiltersHider /> {/* Contains SearchFilters */}
-          <SearchResults />
-        </div>
-      </SearchResultProvider>
+      <SearchFilterProvider>
+        <SearchResultProvider>
+          <div className="p-4 bg-background">
+            <SearchBar />
+            <SearchFiltersHider /> {/* Contains SearchFilters */}
+            <SearchResults />
+          </div>
+        </SearchResultProvider>
+      </SearchFilterProvider>
     </>
   );
 };
