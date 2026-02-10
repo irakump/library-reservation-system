24bd21df1dad1b80640c7f740b988be23c0ae615
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-10 15:02:12 +0200

    add api fetch, add SearchFilterContext to SearchResultContext

--- frontend/src/contexts/SearchResultContext.jsx
+++ frontend/src/contexts/SearchResultContext.jsx
@@ -1,12 +1,24 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from 'react';
+import axios from 'axios';
+import { useSearchFilters } from './SearchFilterContext';
 
 const SearchResultContext = createContext();
 
 export const SearchResultProvider = ({ children }) => {
   const [searchResults, setSearchResults] = useState([]);
+  const { searchFilters } = useSearchFilters();
+
+  const fetchSearchResults = async () => {
+    axios
+      .get('http://localhost:8081/api/book/filter', { params: searchFilters })
+      .then((response) => setSearchResults(response.data))
+      .catch((error) => console.error(error));
+  };
 
   return (
-    <SearchResultContext.Provider value={{ searchResults, setSearchResults }}>
+    <SearchResultContext.Provider
+      value={{ searchResults, setSearchResults, fetchSearchResults }}
+    >
       {children}
     </SearchResultContext.Provider>
   );
