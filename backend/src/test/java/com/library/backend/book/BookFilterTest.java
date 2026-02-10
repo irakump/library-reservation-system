1f75a4e883bdd9b1a797b4c43a55d558974cb2be
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-10 18:50:29 +0200

    add book filter tests

---
+++ backend/src/test/java/com/library/backend/book/BookFilterTest.java
@@ -0,0 +1,119 @@
+package com.library.backend.book;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import com.library.backend.author.Author;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+
+@DataJpaTest
+class BookFilterTest {
+
+    @Autowired
+    BookRepository repository;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    Book book1;
+    Book book2;
+    Book book3;
+    Book book4;
+    Book book5;
+    Book book6;
+
+    @BeforeEach
+    void setUp() {
+        Author author1 = new Author("Author1", "SomeLastName");
+        Author author2 = new Author("Author2", "OtherLastName");
+
+        entityManager.persist(author1);
+        entityManager.persist(author2);
+
+        book1 = new Book("1111", "Test book1", 2021, "Test book", "biography", "english", true);
+        book2 = new Book("2222", "Test book2", 2010, "Test book", "fantasy", "english", true);
+        book3 = new Book("3333", "Test book3", 2004, "Test book", "history", "english", true);
+        book4 = new Book("4444", "Test book4", 2024, "Test book", "history", "finnish", true);
+        book5 = new Book("5555", "Test book5", 2024, "Test book", "fantasy", "english", true);
+        book6 = new Book("6666", "Test book6", 2010, "Test book", "fantasy", "english", true);
+
+        entityManager.persist(book1);
+        entityManager.persist(book2);
+        entityManager.persist(book3);
+        entityManager.persist(book4);
+        entityManager.persist(book5);
+        entityManager.persist(book6);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFilteredByGenre_withGenreHistory_returnBooks() {
+        List<Book> filteredBooks = (List<Book>) repository.findByGenre("history");
+        assertThat(filteredBooks).containsExactly(book3, book4);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFilteredByYear_withYear2010_returnBooks() {
+        List<Book> filteredBooks = (List<Book>) repository.findByYear(2010);
+        assertThat(filteredBooks).containsExactly(book2, book6);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFilteredByLanguage_withLanguageEnglish_returnBooks() {
+        List<Book> filteredBooks = (List<Book>) repository.findByLanguage("english");
+        assertThat(filteredBooks).containsExactly(book1, book2, book3, book5, book6);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFiltered_withGenreHistory_withLanguageEnglish_returnBooks() {
+        List<String> genres = List.of("history");
+        List<Integer> years = null;
+        List<String> languages = List.of("english");
+        Boolean availability = true;
+        String title_author = null;
+
+        List<Book> filteredBooks = (List<Book>) repository.findByFilters(genres, years, languages, availability, title_author);
+        assertThat(filteredBooks).containsExactly(book3);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFiltered_withGenreFantasy_returnBooks() {
+        List<String> genres = List.of("fantasy");
+        List<Integer> years = null;
+        List<String> languages = null;
+        Boolean availability = true;
+        String title_author = null;
+
+        List<Book> filteredBooks = (List<Book>) repository.findByFilters(genres, years, languages, availability, title_author);
+        assertThat(filteredBooks).containsExactly(book2, book5, book6);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFiltered_withGenreBiography_withGenreHistory_returnBooks() {
+        List<String> genres = List.of("biography", "history");
+        List<Integer> years = null;
+        List<String> languages = null;
+        Boolean availability = true;
+        String title_author = null;
+
+        List<Book> filteredBooks = (List<Book>) repository.findByFilters(genres, years, languages, availability, title_author);
+        assertThat(filteredBooks).containsExactly(book1, book3, book4);
+    }
+
+    @Test
+    void givenNewBooks_whenDBFiltered_withAvailableFalse_returnBooks() {
+        List<String> genres = null;
+        List<Integer> years = null;
+        List<String> languages = null;
+        Boolean availability = false;
+        String title_author = null;
+
+        List<Book> filteredBooks = (List<Book>) repository.findByFilters(genres, years, languages, availability, title_author);
+        assertThat(filteredBooks).isEmpty();
+    }
+}
