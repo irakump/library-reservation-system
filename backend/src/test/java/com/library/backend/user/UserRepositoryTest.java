27bfbe9d98063a3346a5252cfd05786b46054741
irakump <ira.kumpula@outlook.com>
2026-02-10 14:52:35 +0200

    Add user related tests

--- backend/src/test/java/com/library/backend/user/UserRepositoryTest.java
+++ backend/src/test/java/com/library/backend/user/UserRepositoryTest.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DataJpaTest
@@ -15,6 +18,15 @@ public class UserRepositoryTest {
 
     @Autowired
     TestEntityManager em;
+
+
+    // Test create operation (insert into test database)
+    @Test
+    void shouldCreateNewUser() {
+        User user = new User("test@email.com", "TestUser", "h8d6s6Gj!230Kh");
+        User insertedUser = repository.save(user);
+        assertThat(em.find(User.class, insertedUser.getUserId())).isEqualTo(user);
+    }
 
     // Test update operation
     @Test
@@ -29,6 +41,54 @@ public class UserRepositoryTest {
 
         User updatedUser = em.find(User.class, newUser.getUserId());
         assertThat(updatedUser.getNickname()).isEqualTo(newNickname);
-
     }
+
+    // Test find all users
+    @Test
+    void shouldFindAllCreatedUsers() {
+        User firstUser = new User("test@email.com", "FirstUser", "h8d6s6Gj!230Kh");
+        User secondUser = new User("test2@email.com", "SecondUser", "h4shhhH");
+        em.persist(firstUser);
+        em.persist(secondUser);
+
+        List<User> users = (List<User>) repository.findAll();
+
+        // Collect user ids to list
+        List<Integer> userIds = users.stream()
+                                .map(User::getUserId)
+                                .toList();
+
+        assertTrue(userIds.contains(firstUser.getUserId()));
+        assertTrue(userIds.contains(secondUser.getUserId()));
+    }
+
+    // Test find by id
+    @Test
+    void shouldFindUserById() {
+        User user = new User("test@email.com", "TestUser", "h8d6s6Gj!230Kh");
+        em.persist(user);
+        Optional<User> fetchedUser = repository.findById(user.getUserId());
+        assertThat(fetchedUser).contains(user);
+    }
+
+    // Test find by id when user id does not exist
+    @Test
+    void shouldNotFindUserByNonExistingId() {
+        User user = new User("test@email.com", "TestUser", "h8d6s6Gj!230Kh");
+        em.persist(user);
+        int nonExistingId = user.getUserId() + 1;
+
+        Optional<User> fetchedUser = repository.findById(nonExistingId);
+        assertTrue(fetchedUser.isEmpty(), "Should not find user with non-existing id");
+    }
+
+    // Test delete operation
+    @Test
+    void shouldDeleteNewUser() {
+        User user = new User("test@email.com", "TestUser", "h8d6s6Gj!230Kh");
+        em.persist(user);
+        repository.delete(user);
+        assertThat(em.find(User.class, user.getUserId())).isNull();
+    }
+
 }
