4fd8c24a4c3108b1ead44237bca1900324101c3e
irakump <ira.kumpula@outlook.com>
2026-02-11 22:13:45 +0200

    Add footer test

---
+++ frontend/src/components/footer/Footer.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Footer from "./Footer";
+import SocialMedia from './SocialMedia';
+import Languages from './Languages';
+
+describe("Footer", () => {
+  it("renders footer component", () => {
+    render(<Footer />);
+
+    screen.debug();
+
+    expect(screen.getByAltText("MetBook logo")).toBeInTheDocument();
+    expect(screen.getByText("Social Media")).toBeInTheDocument();
+    expect(screen.getByText("Language")).toBeInTheDocument();
+  });
+});
