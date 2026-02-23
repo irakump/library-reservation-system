304809892e36860406d4ffa6c57122862176c352
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-23 23:55:19 +0200

    add message to indicate no search results when so

--- frontend/src/components/home/SearchResults.jsx
+++ frontend/src/components/home/SearchResults.jsx
@@ -28,7 +28,7 @@ export const SearchResults = () => {
   }, [searchResults]);
 
   useEffect(() => {
-    searchResultsStartRef.current.focus();
+    //searchResultsStartRef.current.focus();
   }, [currentPage]);
 
   let firstDotsRendered = false;
@@ -37,83 +37,94 @@ export const SearchResults = () => {
   return (
     <>
       {/* Search result header */}
-      <div
-        ref={searchResultsStartRef}
-        className="flex flex-row justify-between mb-2"
-      >
-        <h2>Search Results</h2>
-        <h2>
-          {startIndex + 1} -{' '}
-          {endIndex < searchResults.length ? endIndex : searchResults.length} /{' '}
-          {searchResults.length}
-        </h2>
-      </div>
+      {searchResults.length > 0 ? (
+        <>
+          <div className="flex flex-row justify-between mb-2">
+            <h2 ref={searchResultsStartRef}>Search Results</h2>
+            <h2>
+              {startIndex + 1} -{' '}
+              {endIndex < searchResults.length
+                ? endIndex
+                : searchResults.length}{' '}
+              / {searchResults.length}
+            </h2>
+          </div>
 
-      <div>
-        <BookDataPage
-          title=" "
-          books={searchResults.slice(startIndex, endIndex)}
-          pageType="favourite"
-        />
-      </div>
+          <div>
+            <BookDataPage
+              title=" "
+              books={searchResults.slice(startIndex, endIndex)}
+              pageType="favourite"
+            />
+          </div>
 
-      {/* Search result navigation */}
-      <div className="flex flex-row justify-center items-center mt-4 mb-18 [&>button]:bg-filter [&>button]:p-1.5 [&>button]:rounded-md [&>button]:cursor-pointer">
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentPage((previous) => Math.max(1, previous - 1));
-          }}
-        >
-          {'<'}
-        </button>
+          {/* Search result navigation */}
+          <div className="flex flex-row justify-center items-center mt-4 mb-18 [&>button]:bg-filter [&>button]:p-1.5 [&>button]:rounded-md [&>button]:cursor-pointer">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentPage((previous) => Math.max(1, previous - 1));
+              }}
+            >
+              {'<'}
+            </button>
 
-        {/* Links to search result pages */}
-        <div className="flex flex-row mx-4 [&>a]:mx-1 gap-1">
-          {[...Array(pagesToShow)].map((_, i) => {
-            const page = i + 1;
-            if (
-              page !== 1 &&
-              page !== pagesToShow &&
-              page !== currentPage &&
-              page !== currentPage - 1 &&
-              page !== currentPage + 1
-            ) {
-              if (page < currentPage && !firstDotsRendered) {
-                firstDotsRendered = true;
-                return <p key={'first-nav-dots'}>...</p>;
-              } else if (page > currentPage && !lastDotsRendered) {
-                lastDotsRendered = true;
-                return <p key={'second-nav-dots'}>...</p>;
-              }
-              return;
-            } else {
-              return (
-                <a
-                  key={page}
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setCurrentPage(page);
-                  }}
-                  className={currentPage === page ? 'font-bold ' : undefined}
-                >
-                  {i + 1}
-                </a>
-              );
-            }
-          })}
+            {/* Links to search result pages */}
+            <div className="flex flex-row mx-4 [&>a]:mx-1 gap-1">
+              {[...Array(pagesToShow)].map((_, i) => {
+                const page = i + 1;
+                if (
+                  page !== 1 &&
+                  page !== pagesToShow &&
+                  page !== currentPage &&
+                  page !== currentPage - 1 &&
+                  page !== currentPage + 1
+                ) {
+                  if (page < currentPage && !firstDotsRendered) {
+                    firstDotsRendered = true;
+                    return <p key={'first-nav-dots'}>...</p>;
+                  } else if (page > currentPage && !lastDotsRendered) {
+                    lastDotsRendered = true;
+                    return <p key={'second-nav-dots'}>...</p>;
+                  }
+                  return;
+                } else {
+                  return (
+                    <a
+                      key={page}
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setCurrentPage(page);
+                      }}
+                      className={
+                        currentPage === page ? 'font-bold ' : undefined
+                      }
+                    >
+                      {i + 1}
+                    </a>
+                  );
+                }
+              })}
+            </div>
+
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentPage((previous) =>
+                  Math.min(previous + 1, pagesToShow),
+                );
+              }}
+            >
+              {'>'}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="flex justify-center mb-18">
+          <h2>No Search Results</h2>
         </div>
-
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentPage((previous) => Math.min(previous + 1, pagesToShow));
-          }}
-        >
-          {'>'}
-        </button>
-      </div>
+      )}
     </>
   );
 };
