0b60ffab19ef4e61f54eaa0169be651a08ca6597
nurhana <nur.hana@metropolia.fi>
2026-02-23 14:06:12 +0200

    Add AuthProvider to wrap the component in NavBar.test

--- frontend/src/components/navbar/NavBar.test.jsx
+++ frontend/src/components/navbar/NavBar.test.jsx
@@ -2,20 +2,22 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import NavBar from "./NavBar";
+import { AuthProvider } from "../../contexts/AuthContext";
 import { MenuProvider } from "../../contexts/MenuContext";
 
 describe("NavBar", () => {
   it("renders NavBar component", () => {
     render(
-      <MenuProvider>
-        <NavBar />
-      </MenuProvider>,
+      <AuthProvider>
+        <MenuProvider>
+          <NavBar />
+        </MenuProvider>
+      </AuthProvider>,
     );
 
     screen.debug();
 
     expect(screen.getByText("MetBook")).toBeInTheDocument();
     expect(screen.getByAltText("Stack of books")).toBeInTheDocument();
-
   });
 });
