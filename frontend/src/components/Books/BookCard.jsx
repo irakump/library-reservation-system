c343753106955bc03a99cdc77c905459cf3df20e
nurhana <nur.hana@metropolia.fi>
2026-02-24 12:17:24 +0200

    Add author to book card

--- frontend/src/components/Books/BookCard.jsx
+++ frontend/src/components/Books/BookCard.jsx
@@ -2,45 +2,49 @@ import Button from "./Button";
 import { getPage } from "../../utils/getPage";
 import FavoriteButton from "./FavoriteButton.jsx";
 
-
-
-const BookCard = ({ book, pageType, setOpen, }) => {
-  const page = getPage(pageType, book)
+const BookCard = ({ book, pageType, setOpen }) => {
+  const page = getPage(pageType, book);
 
   return (
-    <div className="bg-white rounded-lg p-2 flex gap-6 shadow hover:shadow-lg hover:opacity-90 transition-all " onClick={() => setOpen(book)}>
-      <div className="w-24 h-32 mt-1.5 ml-1.5 bg-gray-300 rounded shrink-0" ></div>
+    <div
+      className="bg-white rounded-lg p-2 flex gap-6 shadow hover:shadow-lg hover:opacity-90 transition-all "
+      onClick={() => setOpen(book)}
+    >
+      <div className="w-24 h-32 mt-1.5 ml-1.5 bg-gray-300 rounded shrink-0"></div>
 
       <div className="flex-1">
         <div className="flex justify-between items-start">
           <h3 className="font-bold text-lg">{book.title}</h3>
-            <div className="text-2xl cursor-pointer hover:text-red-700">
-                <FavoriteButton
-                book={book}/>
-            </div>
+          <div className="text-2xl cursor-pointer hover:text-red-700">
+            <FavoriteButton book={book} />
+          </div>
         </div>
 
-        <p className="text-sm mb-1 text-left">{book.author}</p>
+        <p className="text-sm mb-1 text-left">
+          {book.authors
+            ?.map((author) => `${author.firstName} ${author.lastName}`)
+            .join(",")}
+        </p>
         <p className="text-sm mb-1 text-left ">{book.year}</p>
         <p className="text-sm mb-1 text-left capitalize">{book.genre}</p>
 
         {page.showDates && (
           <>
-            <p className="text-sm mb-1 text-left">Borrowed: {book.borrowedDate}</p>
-            <p className="text-sm mb-1 text-left">Returned: {book.returnedDate}</p>
+            <p className="text-sm mb-1 text-left">
+              Borrowed: {book.borrowedDate}
+            </p>
+            <p className="text-sm mb-1 text-left">
+              Returned: {book.returnedDate}
+            </p>
           </>
         )}
 
         <p className="text-sm mb-0 text-left">{page.p}</p>
-        
 
         {/* Button for loan/reserve/return jene */}
         {!page.showDates && (
-          <Button onClick={()=> page.action(book)}> {page.BtnText} </Button>
+          <Button onClick={() => page.action(book)}> {page.BtnText} </Button>
         )}
-    
-      
-
       </div>
     </div>
   );
