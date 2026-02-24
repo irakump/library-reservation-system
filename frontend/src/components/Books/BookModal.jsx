eeb60b4ebb98e5872a3adc49ef6c6c7d2843a02d
nurhana <nur.hana@metropolia.fi>
2026-02-24 12:18:16 +0200

    Add author to book modal

--- frontend/src/components/Books/BookModal.jsx
+++ frontend/src/components/Books/BookModal.jsx
@@ -1,81 +1,107 @@
-import Button from "./Button"
+import Button from "./Button";
 import { getPage } from "../../utils/getPage";
 import FavoriteButton from "./FavoriteButton.jsx";
 
-const BookModal = ({book, pageType, setOpen}) => {
-    const page = getPage(pageType, book)
+const BookModal = ({ book, pageType, setOpen }) => {
+  const page = getPage(pageType, book);
 
-//
-    return(
-        <>
-        <div className="fixed inset-0 flex items-start md:items-center justify-center bg-black/40" onClick={() => setOpen(null)}>
-          <div className="relative w-full max-w-[465px] max-h-dvh md:max-h-[95dvh] overflow-y-auto bg-white rounded-xl p-9 border-20 border-filter" 
-          onClick={(e) => e.stopPropagation()}>
-            <button 
-            onClick={() => setOpen(null)} className="absolute top-0 right-4 text-xl">✕</button>
-            <div>
-                <div className="cursor-pointer hover:text-red-700 absolute top-10 right-4 text-4xl">
-                    <FavoriteButton book={book}
-                    ></FavoriteButton>
-                </div>
+  //
+  return (
+    <>
+      <div
+        className="fixed inset-0 flex items-start md:items-center justify-center bg-black/40"
+        onClick={() => setOpen(null)}
+      >
+        <div
+          className="relative w-full max-w-[465px] max-h-dvh md:max-h-[95dvh] overflow-y-auto bg-white rounded-xl p-9 border-20 border-filter"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <button
+            onClick={() => setOpen(null)}
+            className="absolute top-0 right-4 text-xl"
+          >
+            ✕
+          </button>
+          <div>
+            <div className="cursor-pointer hover:text-red-700 absolute top-10 right-4 text-4xl">
+              <FavoriteButton book={book}></FavoriteButton>
+            </div>
 
-              <div className="flex gap-4">
-              <img src={book.image} alt="Book cover" className="w-24 h-36 object-cover"
-                />
+            <div className="flex gap-4">
+              <img
+                src={book.image}
+                alt="Book cover"
+                className="w-24 h-36 object-cover"
+              />
               <div className="flex-1">
-                  <h1 className="font-bold text-lg">{book.title}</h1>
-                  <p className="text-sm mb-1"> {book.author} </p>
-                  <p className="text-sm mb-1">{book.year} </p>
-                  <p className="text-sm mb-1">
-                      {book.description}
-                  </p>
-                </div>
-                </div>
-                <div className="mt-6 text-sm text-gray-700 space-y-1 capitalize">
-                  <p><span className="font-medium">Language:</span> {book.language}</p>
-                  <p><span className="font-medium">Genre:</span> {book.genre}</p>
-                </div>
-                <div className="mt-3 flex items-center gap-2">
-                  <span className="px-3 py-1 text-xs rounded-full bg-gray-100">Finnish</span>
-                  <span className="px-3 py-1 text-xs rounded-full bg-gray-100">History</span>
-                
-                </div>
-                <div className="mt-6 flex items-end justify-between">
-                  <div className="text-sm">
-                    <p className="flex items-center gap-2"></p>
+                <h1 className="font-bold text-lg">{book.title}</h1>
+                <p className="text-sm mb-1">
+                  {book.authors
+                    ?.map((author) => `${author.firstName} ${author.lastName}`)
+                    .join(", ")}
+                </p>
+                <p className="text-sm mb-1">{book.year} </p>
+                <p className="text-sm mb-1">{book.description}</p>
+              </div>
+            </div>
+            <div className="mt-6 text-sm text-gray-700 space-y-1 capitalize">
+              <p>
+                <span className="font-medium">Language:</span> {book.language}
+              </p>
+              <p>
+                <span className="font-medium">Genre:</span> {book.genre}
+              </p>
+            </div>
+            <div className="mt-3 flex items-center gap-2">
+              <span className="px-3 py-1 text-xs rounded-full bg-gray-100">
+                Finnish
+              </span>
+              <span className="px-3 py-1 text-xs rounded-full bg-gray-100">
+                History
+              </span>
+            </div>
+            <div className="mt-6 flex items-end justify-between">
+              <div className="text-sm">
+                <p className="flex items-center gap-2"></p>
 
+                {/*for history page */}
+                {page.showDates && (
+                  <>
+                    <p className="text-sm mb-1 text-left">
+                      Borrowed: {book.borrowedDate}
+                    </p>
+                    <p className="text-sm mb-1 text-left">
+                      Returned: {book.returnedDate}
+                    </p>
+                  </>
+                )}
 
-                    {/*for history page */}
-                    {page.showDates && (
-                      <>
-                        <p className="text-sm mb-1 text-left">Borrowed: {book.borrowedDate}</p>
-                        <p className="text-sm mb-1 text-left">Returned: {book.returnedDate}</p>
-                      </>
-                    )}
+                <p className="text-sm mb-1 text-left">{page.p} </p>
 
-                    <p className="text-sm mb-1 text-left">{page.p} </p>
+                {/*queue*/}
+                {page.BtnText == "Reserve" && (
+                  <>
+                    <p className="mt-1">2 people in queue</p>
+                    <p className="text-gray-500">
+                      Estimated loan date x.x.2026
+                    </p>
+                  </>
+                )}
 
-                    {/*queue*/}
-                    {page.BtnText == "Reserve" && (
-                      <>
-                        <p className="mt-1">2 people in queue</p>
-                        <p className="text-gray-500">Estimated loan date x.x.2026</p>
-                      </>
-                    )}
+                {/* Button for loan/reserve/return jene */}
+              </div>
 
-                    {/* Button for loan/reserve/return jene */}
-                  </div>
-
-                  {!page.showDates && (
-                    <Button onClick={()=> page.action(book)}> {page.BtnText} </Button>
-                  )}
-                 
-                </div>
-            </div>    
+              {!page.showDates && (
+                <Button onClick={() => page.action(book)}>
+                  {" "}
+                  {page.BtnText}{" "}
+                </Button>
+              )}
+            </div>
           </div>
         </div>
-
-        </>
-    )
-}
-export default BookModal
+      </div>
+    </>
+  );
+};
+export default BookModal;
