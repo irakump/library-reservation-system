aa0b7bbab7fd2c8d284d92dc0f01aae10983c991
annimala <minerva.alanen@gmail.com>
2026-02-26 17:23:48 +0200

    added apicalls to frontend and updated loan tests

--- backend/src/main/java/com/library/backend/loan/CreateLoanDTO.java
+++ backend/src/main/java/com/library/backend/loan/CreateLoanDTO.java
@@ -5,25 +5,13 @@ import java.time.LocalDateTime;
 public class CreateLoanDTO {
     private int userId;
     private String isbn;
-    private LocalDateTime dueDate;
 
     public int getUserId() {
         return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
     }
 
     public String getIsbn()  {
         return isbn;
     }
 
-    public LocalDateTime getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueTime(LocalDateTime dueDate) {
-        this.dueDate = dueDate;
-    }
 }
--- backend/src/main/java/com/library/backend/loan/LoanService.java
+++ backend/src/main/java/com/library/backend/loan/LoanService.java
@@ -4,11 +4,10 @@ import com.library.backend.book.Book;
 import com.library.backend.book.BookRepository;
 import com.library.backend.user.User;
 import com.library.backend.user.UserRepository;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Service
 public class LoanService {
@@ -27,7 +26,8 @@ public class LoanService {
         User user = userRepo.findById(dto.getUserId()).orElseThrow(() -> new RuntimeException("user not found: "));
         Book book = bookRepo.findById(dto.getIsbn()).orElseThrow(() -> new RuntimeException("isbn not found: "));
 
-        Loan loan = new Loan(dto.getDueDate(), user, book);
+        LocalDateTime dueDate = LocalDateTime.now().plusWeeks(2);
+        Loan loan = new Loan(dueDate, user, book);
         loanRepo.save(loan);
     }
 }
