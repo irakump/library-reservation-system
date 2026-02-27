7b795b7a74c2bf4b990256ed0c1e627c6a58e1bb
nurhana <nur.hana@metropolia.fi>
2026-02-27 09:46:27 +0200

    Fixed failed 401 Unauthorized controller tests

--- backend/src/test/java/com/library/backend/language/LanguageControllerTest.java
+++ backend/src/test/java/com/library/backend/language/LanguageControllerTest.java
@@ -3,6 +3,7 @@ package com.library.backend.language;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(LanguageController.class)
+@WithMockUser
 public class LanguageControllerTest {
 
     @Autowired
