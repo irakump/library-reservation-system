540c31a865d05edb3c79e12364e3b5374ffba794
irakump <ira.kumpula@outlook.com>
2026-03-02 22:35:23 +0200

    Set timestamp and status to reservation at creation

--- backend/src/main/java/com/library/backend/reservation/Reservation.java
+++ backend/src/main/java/com/library/backend/reservation/Reservation.java
@@ -3,6 +3,8 @@ package com.library.backend.reservation;
 import com.library.backend.user.User;
 import com.library.backend.book.Book;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.sql.Timestamp;
 
 @Entity
@@ -14,12 +16,13 @@ public class Reservation {
     @Column(name = "reservation_id")
     private int reservationId;
 
-    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP", updatable = false)
     private Timestamp createdAt;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
-    private Status status;
+    private Status status = Status.active;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
@@ -48,6 +51,10 @@ public class Reservation {
         return status;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public User getUser() {
         return user;
     }
@@ -67,4 +74,5 @@ public class Reservation {
     public enum Status {
         active, not_active
     }
+
 }
