9b978cdff43e31ff8338a81cd61d6d0b17921665
irakump <ira.kumpula@outlook.com>
2026-03-03 12:23:54 +0200

    feat: cancel reservation

--- frontend/src/components/buttons/ReserveButton.jsx
+++ frontend/src/components/buttons/ReserveButton.jsx
@@ -1,26 +1,38 @@
-
 //this components is rendered inside BookButton component which gets rendered inside BookModal and BookCard
 
 import { useReservationContext } from "../../contexts/ReservationContext";
 
-function ReserveButton({pageType, book, children}) {
-    const {addToReservations, updateReservationStatus} = useReservationContext();
+function ReserveButton({ pageType, book, children }) {
+  const { addToReservations, updateReservationStatus, reservations } =
+    useReservationContext();
 
-    // New reservation
-    return (
-        <>
-            <p className="text-sm mb-0 text-left"> 🔴 Not available</p>
-            <button className="bg-filter font-semibold rounded-xl px-6 py-2 hover:bg-sky-500 float-right cursor-pointer"
-                    onClick={e => {
-                        e.stopPropagation();
-                        addToReservations(book.isbn);
-                        {/*here reserve function call in context or something*/}
-                    }}>
-                {children}
-            </button>
-        </>
-    );
-    
-} 
+  // Check active reservation
+  const activeReservation = reservations.find(
+    (r) => r.bookIsbn === (book.isbn ?? book.bookIsbn) && r.status === "active",
+  );
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (pageType === "reservation" && activeReservation) {
+      updateReservationStatus(activeReservation.reservationId);
+    } else {
+        const isbn = book.isbn ?? book.bookIsbn;    // If book.isbn is null, use book.bookIsbn
+      addToReservations(isbn);
+    }
+  };
+
+  // New reservation
+  return (
+    <>
+      <p className="text-sm mb-0 text-left"> 🔴 Not available</p>
+      <button
+        className="bg-filter font-semibold rounded-xl px-6 py-2 hover:bg-sky-500 float-right cursor-pointer"
+        onClick={handleClick}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
 
 export default ReserveButton;
