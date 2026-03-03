81d79b5712716581d1ecffea7ba691552c66b2cb
irakump <ira.kumpula@outlook.com>
2026-03-03 13:46:08 +0200

    feat: favorite books in reservation page Update FavoriteButton to handle reservation's bookIsbn field

--- frontend/src/components/buttons/FavoriteButton.jsx
+++ frontend/src/components/buttons/FavoriteButton.jsx
@@ -3,17 +3,24 @@ import { useFavoritesContext } from "../../contexts/FavoritesContext.jsx";
 const FavoriteButton = ({ book }) => {
   const { isFavorite, addToFavorites, removeFromfavorites } =
     useFavoritesContext();
-  let favorite = isFavorite(book.isbn);
-  //console.log(("Removefav: ", removeFromfavorites))
 
-    function onFavoriteClick(e) {
-        e.stopPropagation();
-        if (favorite) removeFromfavorites(book.isbn)
-        else addToFavorites(book)
+  const isbn = book.isbn ?? book.bookIsbn; // With reservations, use bookIsbn
+
+  const favorite = isFavorite(isbn);
+
+  function onFavoriteClick(e) {
+    e.stopPropagation();
+
+    if (favorite) {
+      removeFromfavorites(book.isbn ?? book.bookIsbn);
+    } else {
+      addToFavorites({ ...book, isbn }); // New book object to include also reservations bookIsbn as isbn
     }
+  }
 
   return (
     <button onClick={(e) => onFavoriteClick(e)}>{favorite ? "♥︎" : "♡"}</button>
   );
 };
+
 export default FavoriteButton;
