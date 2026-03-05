f7889200b3b33af37fdfd642f700d2a12ea76a00
irakump <ira.kumpula@outlook.com>
2026-03-05 15:12:40 +0200

    Alert user when loaning or returning books

--- frontend/src/contexts/LoanContext.jsx
+++ frontend/src/contexts/LoanContext.jsx
@@ -1,5 +1,10 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {createLoan, getHistory, getLoans, returnLoan} from "../api/loansApi.jsx";
+import {
+  createLoan,
+  getHistory,
+  getLoans,
+  returnLoan,
+} from "../api/loansApi.jsx";
 import { useAuth } from "./AuthContext.jsx";
 
 const LoanContext = createContext({});
@@ -9,7 +14,7 @@ export const useLoanContext = () => useContext(LoanContext);
 export const LoanProvider = ({ children }) => {
   const [loans, setLoans] = useState([]);
   const { user, isLoggedIn } = useAuth();
-  const [history, setHistory] = useState([])
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
     // Only fetch if user is logged in
@@ -23,11 +28,14 @@ export const LoanProvider = ({ children }) => {
   const addToLoans = async (isbn) => {
     if (!user?.userId) {
       console.error("User not logged in");
+      alert("Please log in to loan or reserve books");
       return;
     }
+
     try {
       const response = await createLoan(user.userId, isbn); //Use JWT userId
-        setLoans((prev) => [...prev, response]);
+      setLoans((prev) => [...prev, response]);
+      alert(`${response.title} loaned`);
     } catch (error) {
       console.error("Error creating loan: ", error);
     }
@@ -35,27 +43,30 @@ export const LoanProvider = ({ children }) => {
 
   const removeLoans = async (userId, isbn, loanId) => {
     try {
-      await returnLoan(userId, isbn, loanId);
-      setLoans(prev => prev.filter(f => f.loanId !== loanId));
+      const loan = loans.find((l) => l.loanId === loanId); // Find loan to get book title
+      const title = loan.title || "Book"; // Fallback title is Book
 
+      await returnLoan(userId, isbn, loanId);
+      setLoans((prev) => prev.filter((f) => f.loanId !== loanId));
+      alert(`${title} returned`);
     } catch (error) {
       console.error("Error returning loan: ", error);
+      alert("Error while returning loan. Please try again.")
     }
   };
 
-    useEffect(() => {
-        if (isLoggedIn && user?.userId)
-            getHistory(user.userId)
-                .then((res) => setHistory(res.data))
-                .catch((error) => console.error(error))
-    }, [isLoggedIn, user]);
-
+  useEffect(() => {
+    if (isLoggedIn && user?.userId)
+      getHistory(user.userId)
+        .then((res) => setHistory(res.data))
+        .catch((error) => console.error(error));
+  }, [isLoggedIn, user]);
 
   const value = {
     addToLoans,
     removeLoans,
     loans,
-      history,
+    history,
   };
 
   return <LoanContext.Provider value={value}>{children}</LoanContext.Provider>;
