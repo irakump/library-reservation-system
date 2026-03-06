3a07a439a551ce8f0fe13575ac54455d87d45f29
irakump <ira.kumpula@outlook.com>
2026-03-06 11:36:24 +0200

    Make title ang image responsive

--- frontend/src/components/Books/BookModal.jsx
+++ frontend/src/components/Books/BookModal.jsx
@@ -16,7 +16,6 @@ const BookModal = ({ book, pageType, setOpen, addToLoans }) => {
     }
   }, [isbn, book.availability]);
 
-
   return (
     <>
       <div
@@ -24,7 +23,7 @@ const BookModal = ({ book, pageType, setOpen, addToLoans }) => {
         onClick={() => setOpen(null)}
       >
         <div
-          className="relative w-full max-w-[465px] max-h-dvh md:max-h-[95dvh] overflow-y-auto bg-white rounded-xl p-9 border-20 border-filter"
+          className="relative w-full max-w-[465px] max-h-dvh md:max-h-[95dvh] overflow-y-auto bg-white rounded-xl p-6 sm:p-9 border-20 border-filter"
           onClick={(e) => e.stopPropagation()}
         >
           <button
@@ -33,22 +32,28 @@ const BookModal = ({ book, pageType, setOpen, addToLoans }) => {
           >
             ✕
           </button>
-          <div>
-            <div className="absolute cursor-pointer hover:text-red-700 top-10 right-4 text-3xl ">
+          <div className="flex flex-col">
+            <div className="absolute cursor-pointer hover:text-red-700 sm:top-10 right-4 text-3xl">
               <FavoriteButton book={book}></FavoriteButton>
             </div>
 
-            <div className="flex gap-4">
-              <div className="w-24 h-auto mt-1 mr-0.5 shrink-0">
-                <img
-                  src={`/books/${pageType === "reservation" ? book.bookIsbn : book.isbn}.jpg`}
-                  alt={`Book image for ${book.title}`}
-                  className="w-24 h-auto rounded-sm outline-1 outline-gray-200"
-                />
+            <div className="flex flex-col gap-4 ">
+              <div className="flex flex-col-reverse sm:flex-row">
+                <div className="w-36 sm:w-24 h-auto mt-1 mr-0.5 shrink-0 max-sm:self-center">
+                  <img
+                    src={`/books/${pageType === "reservation" ? book.bookIsbn : book.isbn}.jpg`}
+                    alt={`Book image for ${book.title}`}
+                    className="w-36 sm:w-24 h-auto rounded-sm outline-1 outline-gray-200"
+                  />
+                </div>
+                <div className="max-sm:self-center">
+                  <h1 className="font-bold text-lg text-left p-3 pt-0 pr-6 sm:pr-2 sm:ml-3">
+                    {book.title}
+                  </h1>
+                </div>
               </div>
 
               <div className="flex-1">
-                <h1 className="font-bold text-lg pr-2">{book.title}</h1>
                 <p className="text-sm mb-1">
                   {book.authors
                     ?.map((author) => `${author.firstName} ${author.lastName}`)
@@ -58,9 +63,7 @@ const BookModal = ({ book, pageType, setOpen, addToLoans }) => {
                 <p className="text-sm mb-1">{book.description}</p>
               </div>
             </div>
-            <div className="mt-6 text-sm text-gray-700 space-y-1 capitalize">
-              
-            </div>
+            <div className="mt-6 text-sm text-gray-700 space-y-1 capitalize"></div>
             <div className="mt-3 flex items-center gap-2 capitalize">
               <span className="px-3 py-1 text-xs rounded-full bg-gray-100">
                 {book.language}
@@ -74,7 +77,6 @@ const BookModal = ({ book, pageType, setOpen, addToLoans }) => {
                 {book.availability === false && queueLength !== null && (
                   <p>Queue length: {queueLength}</p>
                 )}
-
               </div>
               {/* Button for loan/reserve/return/history data on history page */}
               <BookButtons pageType={pageType} book={book}></BookButtons>
