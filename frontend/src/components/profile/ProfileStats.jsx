45f3455abe3bdacbb022f7f8ea5b75a1f37ab27b
nurhana <nur.hana@metropolia.fi>
2026-03-07 00:16:21 +0200

    Improve responsiveness in profileStats

--- frontend/src/components/profile/ProfileStats.jsx
+++ frontend/src/components/profile/ProfileStats.jsx
@@ -11,14 +11,19 @@ const ProfileStats = () => {
 
   return (
     <>
-      <div className="bg-white rounded-lg shadow-sm">
+      <div className="bg-white rounded-lg shadow-sm h-fit">
         <div className=" divide-y-2 divide-blue-200">
           {stats.map((info) => (
-            <div key={info.id} className="flex gap-4 py-3 items-center">
-              <span className="w-36 text-sm font-semibold text-left ml-4">
+            <div
+              key={info.id}
+              className="flex gap-4 py-3 sm:py-4 items-center px-4 sm:px-6"
+            >
+              <span className="w-36 sm:w-44 text-sm sm:text-base font-semibold text-left ml-4">
                 {info.label}
               </span>
-              <span className="text-sm font-medium">{info.value}</span>
+              <span className="text-sm sm:text-base font-medium">
+                {info.value}
+              </span>
             </div>
           ))}
         </div>
