acbab6eca00ff5c2fcfae96de1dc6c63180317f5
nurhana <nur.hana@metropolia.fi>
2026-03-06 19:18:37 +0200

    fix login modal drag and release bug

--- frontend/src/components/auth/LoginModal.jsx
+++ frontend/src/components/auth/LoginModal.jsx
@@ -58,12 +58,13 @@ const LoginModal = ({ isOpen, onClose, onSwitchToRegister }) => {
   return (
     <div
       className="fixed inset-0  bg-black/40 flex items-center justify-center z-50 p-4"
-      onClick={onClose}
+      onMouseDown={(e) => {
+        if (e.target === e.currentTarget) {
+          onClose();
+        }
+      }}
     >
-      <div
-        className="bg-white rounded-lg max-w-md w-full max-h-[90vh] flex flex-col overflow-hidden"
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="bg-white rounded-lg max-w-md w-full max-h-[90vh] flex flex-col overflow-hidden">
         {/* Header */}
         <div className="bg-navbar p-4 flex justify-between text-center flex-0">
           <h2 className="text-3xl font-bold text-gray-800">Login</h2>
