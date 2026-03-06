8507c6a971baf0216635456256b03f7299c9f592
nurhana <nur.hana@metropolia.fi>
2026-03-06 14:58:12 +0200

    Remove one console.log from BookDataPage.jsx

--- frontend/src/pages/BookDataPage.jsx
+++ frontend/src/pages/BookDataPage.jsx
@@ -7,13 +7,10 @@ const BookDataPage = ({ title, books, pageType }) => {
   const [open, setOpen] = useState(null); //passes bookobject
   const { addToLoans } = useLoanContext();
 
-  console.log(books);
   return (
     <>
       <div>
-        <h1 className="text-2xl font-bold text-center text-heading">
-          {title}
-        </h1>
+        <h1 className="text-2xl font-bold text-center text-heading">{title}</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 bg-profileBackground p-6 gap-4 sm:rounded-md">
           {books.map((book) => (
             <BookCard
