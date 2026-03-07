1d89624a4cb7d416c48b1cb1b53ea529ed2a8f51
nurhana <nur.hana@metropolia.fi>
2026-03-07 10:26:53 +0200

    Remove empty fragments from ProfileStats.jsx, UserInformation.jsx and ProfilePage.jsx since returning one root element

--- frontend/src/components/profile/ProfileStats.jsx
+++ frontend/src/components/profile/ProfileStats.jsx
@@ -10,25 +10,23 @@ const ProfileStats = () => {
   ];
 
   return (
-    <>
-      <div className="bg-white rounded-lg shadow-sm h-fit">
-        <div className=" divide-y-2 divide-blue-200">
-          {stats.map((info) => (
-            <div
-              key={info.id}
-              className="flex gap-4 py-3 sm:py-4 items-center px-4 sm:px-6"
-            >
-              <span className="w-36 sm:w-44 text-sm sm:text-base font-semibold text-left ml-4">
-                {info.label}
-              </span>
-              <span className="text-sm sm:text-base font-medium">
-                {info.value}
-              </span>
-            </div>
-          ))}
-        </div>
+    <div className="bg-white rounded-lg shadow-sm h-fit">
+      <div className=" divide-y-2 divide-blue-200">
+        {stats.map((info) => (
+          <div
+            key={info.id}
+            className="flex gap-4 py-3 sm:py-4 items-center px-4 sm:px-6"
+          >
+            <span className="w-36 sm:w-44 text-sm sm:text-base font-semibold text-left ml-4">
+              {info.label}
+            </span>
+            <span className="text-sm sm:text-base font-medium">
+              {info.value}
+            </span>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -5,22 +5,20 @@ import { useAuth } from "../contexts/AuthContext";
 const ProfilePage = () => {
   const { user, isLoggedIn } = useAuth();
   return (
-    <>
-      <div className="min-h-screen flex flex-col">
-        <main className="flex-1 bg-background pb-12">
-          <h1 className="text-2xl sm:text-3xl font-bold text-center text-heading p-7 mb-2 ">
-            Hello, {isLoggedIn && user ? user.nickname : "Guest"}
-          </h1>
-          <div className="mx-auto px-4 max-w-md sm:max-w-lg lg:max-w-2xl">
-            <div className="space-y-6 bg-profileBackground rounded-lg p-4 sm:p-6 ">
-              <ProfileStats />
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 bg-background pb-12">
+        <h1 className="text-2xl sm:text-3xl font-bold text-center text-heading p-7 mb-2 ">
+          Hello, {isLoggedIn && user ? user.nickname : "Guest"}
+        </h1>
+        <div className="mx-auto px-4 max-w-md sm:max-w-lg lg:max-w-2xl">
+          <div className="space-y-6 bg-profileBackground rounded-lg p-4 sm:p-6 ">
+            <ProfileStats />
 
-              <UserInformation />
-            </div>
+            <UserInformation />
           </div>
-        </main>
-      </div>
-    </>
+        </div>
+      </main>
+    </div>
   );
 };
 
