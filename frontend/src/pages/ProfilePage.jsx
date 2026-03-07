2d9e68a0e35f14f9b540b5ae3e84158fb61aac4a
nurhana <nur.hana@metropolia.fi>
2026-03-07 10:39:09 +0200

    Adjust padding in Profile page title

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -7,7 +7,7 @@ const ProfilePage = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-1 bg-background pb-12">
-        <h1 className="text-2xl sm:text-3xl font-bold text-center text-heading p-7 mb-2 ">
+        <h1 className="text-2xl sm:text-3xl font-bold text-center text-heading py-6 sm:py-8">
           Hello, {isLoggedIn && user ? user.nickname : "Guest"}
         </h1>
         <div className="mx-auto px-4 max-w-md sm:max-w-lg lg:max-w-2xl">
