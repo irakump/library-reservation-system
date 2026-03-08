b1b073de35908e2990cd0577fd35d55122dc4d46
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-08 16:19:12 +0200

    move filter container logic to Home.jsx, improve padding and margin styles

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,29 @@
-import Footer from "../components/footer/Footer";
-import SearchBar from "../components/home/SearchBar";
-import SearchResults from "../components/home/SearchResults";
-import SearchFiltersHider from "../components/home/SearchFiltersHider";
+import { useState } from 'react';
 
-import { SearchResultProvider } from "../contexts/SearchResultContext";
-import { SearchFilterProvider } from "../contexts/SearchFilterContext";
+import SearchBar from '../components/home/SearchBar';
+import SearchResults from '../components/home/SearchResults';
+import SearchFiltersHider from '../components/home/SearchFiltersHider';
+
+import { SearchResultProvider } from '../contexts/SearchResultContext';
+import { SearchFilterProvider } from '../contexts/SearchFilterContext';
 
 const Home = () => {
+  const [filtersVisible, setFiltersVisible] = useState(true);
+
+  const toggleFilters = () => {
+    setFiltersVisible(!filtersVisible);
+  };
+
   return (
     <>
       <SearchFilterProvider>
         <SearchResultProvider>
-          <div className="sm:p-4 bg-background pt-10 pb-5">
-            <SearchBar />
-            <SearchFiltersHider /> {/* Contains SearchFilters */}
-            <SearchResults />
+          <div className="bg-background py-10 sm:py-20">
+            <div className="flex flex-col gap-16 sm:gap-20 mx-auto sm:max-w-4xl sm:px-3">
+              <SearchBar />
+              <SearchFiltersHider filtersVisible={filtersVisible} toggleFilters={toggleFilters}/> {/* Contains SearchFilters */}
+              <SearchResults />
+            </div>
           </div>
         </SearchResultProvider>
       </SearchFilterProvider>
