8750a39a70cc67a8d1f37917af4f995d02029458
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-08 16:55:42 +0200

    wrap navbar rendering inside MemoryRouter so NavBar.test.jsx passes

--- frontend/src/components/navbar/NavBar.test.jsx
+++ frontend/src/components/navbar/NavBar.test.jsx
@@ -4,15 +4,18 @@ import { describe, expect, it } from "vitest";
 import NavBar from "./NavBar";
 import { AuthProvider } from "../../contexts/AuthContext";
 import { MenuProvider } from "../../contexts/MenuContext";
+import { MemoryRouter } from "react-router";
 
 describe("NavBar", () => {
   it("renders NavBar component", () => {
     render(
-      <AuthProvider>
-        <MenuProvider>
-          <NavBar />
-        </MenuProvider>
-      </AuthProvider>,
+      <MemoryRouter>
+        <AuthProvider>
+          <MenuProvider>
+            <NavBar />
+          </MenuProvider>
+        </AuthProvider>
+      </MemoryRouter>,
     );
 
     //screen.debug();
