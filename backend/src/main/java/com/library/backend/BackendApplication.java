527331683f34c5ac5f7d8e0399c1d90a7013ff71
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-08 16:09:39 +0200

    remove unused code from BackendApplication.java

--- backend/src/main/java/com/library/backend/BackendApplication.java
+++ backend/src/main/java/com/library/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.library.backend;
 
-import com.library.backend.genre.GenreRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,19 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class);
     }
-
-    /*
-    @Bean
-    public CommandLineRunner testGenre(GenreRepository repository) {
-        return args -> {
-            System.out.println("Genres in database:");
-            repository.findAll().forEach(genre -> System.out.println(genre.getGenre()));
-        };
-    }
-
-     */
 }
