d22b4946a5685577addf25c873ed44d1a3f27797
nurhana <nur.hana@metropolia.fi>
2026-03-08 10:03:09 +0200

    Remove max-width from BookDataPage, user pages will handle their width, as Bookdatapage is used also in SearchResults

--- frontend/src/pages/BookDataPage.jsx
+++ frontend/src/pages/BookDataPage.jsx
@@ -10,7 +10,11 @@ const BookDataPage = ({ title, books, pageType }) => {
   return (
     <>
       <div className="sm:max-w-4xl mx-auto w-full">
-        <h1 className={`text-2xl font-bold text-center text-heading ${pageType === "favourite" ? "py-0" : "py-6"}`}>{title}</h1>
+        <h1
+          className={`text-2xl font-bold text-center text-heading ${pageType === "favourite" ? "py-0" : "py-6"}`}
+        >
+          {title}
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 bg-profileBackground p-6 gap-4 sm:rounded-md">
           {books.map((book) => (
             <BookCard
