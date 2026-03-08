f6724bfccf870ea217888b806a3a1b13829608b5
nurhana <nur.hana@metropolia.fi>
2026-03-08 10:28:17 +0200

    Add responsive container padding, gridgap and removed sm: from rounded-md now always rounded and consistence

--- frontend/src/pages/BookDataPage.jsx
+++ frontend/src/pages/BookDataPage.jsx
@@ -15,7 +15,7 @@ const BookDataPage = ({ title, books, pageType }) => {
         >
           {title}
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 bg-profileBackground p-6 gap-4 sm:rounded-md">
+        <div className="grid grid-cols-1 md:grid-cols-2 bg-profileBackground p-4 sm:p-6 gap-4 sm:gap-6 rounded-md">
           {books.map((book) => (
             <BookCard
               key={book.isbn}
