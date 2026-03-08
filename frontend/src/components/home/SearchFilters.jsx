9df9f80d168f12640a1b9b22a8b174cfd437175f
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-08 16:26:39 +0200

    modify styling in SearchFilters.jsx

--- frontend/src/components/home/SearchFilters.jsx
+++ frontend/src/components/home/SearchFilters.jsx
@@ -1,8 +1,8 @@
-import ActiveFilters from "./ActiveFilters";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { useSearchResult } from "../../contexts/SearchResultContext.jsx";
-import { useSearchFilters } from "../../contexts/SearchFilterContext.jsx";
+import ActiveFilters from './ActiveFilters';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSearchResult } from '../../contexts/SearchResultContext.jsx';
+import { useSearchFilters } from '../../contexts/SearchFilterContext.jsx';
 
 const SearchFilters = () => {
   // context
@@ -16,25 +16,25 @@ const SearchFilters = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8081/api/genre")
+      .get('http://localhost:8081/api/genre')
       .then((response) => setGenres(response.data))
       .catch((error) => console.error(error));
 
     axios
-      .get("http://localhost:8081/api/language")
+      .get('http://localhost:8081/api/language')
       .then((response) => setLanguages(response.data))
       .catch((error) => console.error(error));
 
     axios
-      .get("http://localhost:8081/api/book/years")
+      .get('http://localhost:8081/api/book/years')
       .then((response) => setYears(response.data))
       .catch((error) => console.error(error));
   }, []);
 
   const filterTypes = {
-    genre: "genre",
-    language: "language",
-    years: "years",
+    genre: 'genre',
+    language: 'language',
+    years: 'years',
   };
 
   // option to limit genre and language filters for one per category
@@ -135,130 +135,128 @@ const SearchFilters = () => {
     }
   };
 
-  const [categoryValue, setCategoryValue] = useState("");
+  const [categoryValue, setCategoryValue] = useState('');
 
   const handleValueSelect = (e) => {
     addFilter(e.target.value, e.target.name);
-    setCategoryValue("");
+    setCategoryValue('');
   };
 
   return (
-    <>
-      <div className="flex flex-col items-start [&>div]:w-full">
-        <p className="ml-4 sm:ml-1">Search Filters</p>
+    <div className="flex flex-col items-start [&>div]:w-full">
+      <p className="ml-4 sm:ml-1 mb-2">Search Filters</p>
 
-        <div className="flex flex-col gap-5 bg-profileBackground p-4 sm:rounded-md mb-1">
-          <div className="flex items-start">
-            {/* Availability filter */}
-            <div className="flex items-center gap-2 *:cursor-pointer">
-              <label htmlFor="availability">Available:</label>
-              <input
-                type="checkbox"
-                id="availability"
-                name="availability"
-                checked={available}
-                onChange={(e) => setAvailable(e.target.checked)}
-              />
-            </div>
+      <div className="flex flex-col gap-5 bg-profileBackground p-4 sm:rounded-md mb-1">
+        <div className="flex items-start">
+          {/* Availability filter */}
+          <div className="flex items-center gap-2 *:cursor-pointer">
+            <label htmlFor="availability">Available:</label>
+            <input
+              type="checkbox"
+              id="availability"
+              name="availability"
+              checked={available}
+              onChange={(e) => setAvailable(e.target.checked)}
+            />
           </div>
-
-          <div className="flex flex-col gap-4 sm:flex-row sm:gap-14 [&>div]:sm:flex-1 [&>div>label]:mb-3 [&>div>label]:flex [&>div>label]:flex-col [&>div>label]:gap-1 [&>div>label]:items-start [&>div>label>select]:bg-filter [&>div>label>select]:rounded-sm [&>div>label>select]:border [&>div>label>select]:border-filterBorder [&>div>label>select]:cursor-pointer [&>div>label>select]:w-60 sm:[&>div>label>select]:w-full">
-            {/* Language filter */}
-            <div>
-              <label htmlFor="language">
-                Language
-                <select
-                  className={"capitalize"}
-                  id="language"
-                  name="language"
-                  value={categoryValue}
-                  onChange={handleValueSelect}
-                >
-                  <option value="" disabled>
-                    Select Language
-                  </option>
-                  {languages.map((item) => (
-                    <option
-                      className={"capitalize"}
-                      key={item.language}
-                      value={item.language}
-                      disabled={selectedLanguages.includes(item.language)}
-                    >
-                      {item.language}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-
-            {/* Genre filter */}
-            <div>
-              <label htmlFor="genre">
-                Genre
-                <select
-                  className={"capitalize"}
-                  id="genre"
-                  name="genre"
-                  value={categoryValue}
-                  onChange={handleValueSelect}
-                >
-                  <option value="" disabled>
-                    Select Genre
-                  </option>
-                  {genres.map((item) => (
-                    <option
-                      className={"capitalize"}
-                      key={item.genre}
-                      value={item.genre}
-                      disabled={selectedGenres.includes(item.genre)}
-                    >
-                      {item.genre}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-
-            {/* Year filter */}
-            <div>
-              <label htmlFor="year">
-                Year
-                <select
-                  id="year"
-                  name="years"
-                  value={categoryValue}
-                  onChange={handleValueSelect}
-                >
-                  <option value="" disabled>
-                    Select Year
-                  </option>
-                  {years
-                    .sort((a, b) => b - a)
-                    .map((item) => (
-                      <option
-                        key={item}
-                        value={item}
-                        disabled={selectedYears.includes(item)}
-                      >
-                        {item}
-                      </option>
-                    ))}
-                </select>
-              </label>
-            </div>
-          </div>
-
-          <ActiveFilters
-            filters={{
-              [filterTypes.genre]: selectedGenres,
-              [filterTypes.language]: selectedLanguages,
-              [filterTypes.years]: selectedYears,
-            }}
-            onRemove={removeFilter}
-          />
         </div>
+
+        <div className="flex flex-col gap-4 sm:flex-row sm:gap-14 [&>div]:sm:flex-1 [&>div>label]:mb-3 [&>div>label]:flex [&>div>label]:flex-col [&>div>label]:gap-1 [&>div>label]:items-start [&>div>label>select]:bg-filter [&>div>label>select]:rounded-sm [&>div>label>select]:border [&>div>label>select]:border-filterBorder [&>div>label>select]:cursor-pointer [&>div>label>select]:w-60 sm:[&>div>label>select]:w-full">
+          {/* Language filter */}
+          <div>
+            <label htmlFor="language">
+              Language
+              <select
+                className={'capitalize'}
+                id="language"
+                name="language"
+                value={categoryValue}
+                onChange={handleValueSelect}
+              >
+                <option value="" disabled>
+                  Select Language
+                </option>
+                {languages.map((item) => (
+                  <option
+                    className={'capitalize'}
+                    key={item.language}
+                    value={item.language}
+                    disabled={selectedLanguages.includes(item.language)}
+                  >
+                    {item.language}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+
+          {/* Genre filter */}
+          <div>
+            <label htmlFor="genre">
+              Genre
+              <select
+                className={'capitalize'}
+                id="genre"
+                name="genre"
+                value={categoryValue}
+                onChange={handleValueSelect}
+              >
+                <option value="" disabled>
+                  Select Genre
+                </option>
+                {genres.map((item) => (
+                  <option
+                    className={'capitalize'}
+                    key={item.genre}
+                    value={item.genre}
+                    disabled={selectedGenres.includes(item.genre)}
+                  >
+                    {item.genre}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+
+          {/* Year filter */}
+          <div>
+            <label htmlFor="year">
+              Year
+              <select
+                id="year"
+                name="years"
+                value={categoryValue}
+                onChange={handleValueSelect}
+              >
+                <option value="" disabled>
+                  Select Year
+                </option>
+                {years
+                  .sort((a, b) => b - a)
+                  .map((item) => (
+                    <option
+                      key={item}
+                      value={item}
+                      disabled={selectedYears.includes(item)}
+                    >
+                      {item}
+                    </option>
+                  ))}
+              </select>
+            </label>
+          </div>
+        </div>
+
+        <ActiveFilters
+          filters={{
+            [filterTypes.genre]: selectedGenres,
+            [filterTypes.language]: selectedLanguages,
+            [filterTypes.years]: selectedYears,
+          }}
+          onRemove={removeFilter}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
