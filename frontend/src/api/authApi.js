dd5ddedf7babd155af4188261ec4a2f874d9873f
irakump <ira.kumpula@outlook.com>
2026-03-09 19:56:41 +0200

    Remove comment

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// This is actually authService between frontend and backend
-
 const AUTH_API_BASE_URL = "http://localhost:8081/api/auth";
 
 export const registerAPICall = (registerObj) =>
