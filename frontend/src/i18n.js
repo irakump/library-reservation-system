c2e5248c50c00c40ba658897be16a23d9d7a54ef
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-20 21:37:32 +0200

    modify i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,33 +1,33 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import authEn from '../public/locales/en/auth.json';
-/* import bookCardEn from '../public/locales/en/book_card.json';
-import navigationEn from '../public/locales/en/navigation.json';
-import profileEn from '../public/locales/en/profile.json';
-import searchEn from '../public/locales/en/search.json'; */
-
-const resources = {
-  en: {
-    auth: authEn,
-    /* book_card: bookCardEn,
-    navigation: navigationEn,
-    profile: profileEn,
-    search: searchEn, */
-  },
-};
+import Backend from 'i18next-http-backend';
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
+  .use(initReactI18next)
   .init({
-    resources,
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
+    fallbackLng: 'en',
+    lng: 'en',
+    preload: ['en'],
 
-    ns: ['auth'],
-    defaultNS: 'auth',
+    backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' },
+    ns: [
+      'auth',
+      'book_card',
+      'button',
+      'common',
+      'navigation',
+      'profile',
+      'search',
+    ],
+    defaultNS: 'common',
 
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
     },
   });
+
+export default i18n;
