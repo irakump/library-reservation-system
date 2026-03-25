5ca0a97bbb08a25c559b317aa82e33dd5b907514
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-25 20:15:02 +0200

    add translations for search bar placeholder text and clear filters button

--- frontend/src/components/home/ActiveFilters.jsx
+++ frontend/src/components/home/ActiveFilters.jsx
@@ -1,12 +1,14 @@
+import { useTranslation } from 'react-i18next';
 import FilterTag from './FilterTag';
 
 const ActiveFilters = ({ filters, onRemove }) => {
+  const { t } = useTranslation("search");
   return (
     <>
       <div className="flex flex-wrap gap-2">
         {/* remove all filters */}
         <FilterTag
-          filterName={`Clear Filters`}
+          filterName={t("filters.clear_filters_btn")}
           closeButton={false}
           onRemove={() => onRemove(null, null)}
         />
