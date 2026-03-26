33a98da66442183ba9a20422b27ab495cc992810
irakump <ira.kumpula@outlook.com>
2026-03-26 18:58:27 +0200

    Close any open menus when clicking login/logout

--- frontend/src/components/navbar/LoginButton.jsx
+++ frontend/src/components/navbar/LoginButton.jsx
@@ -1,18 +1,33 @@
 import { useTranslation } from "react-i18next";
 import { useAuth } from "../../contexts/AuthContext";
+import { useMenu } from "../../contexts/MenuContext";
 
 const LoginButton = () => {
   const { isLoggedIn, openLogin, openLogout } = useAuth();
+  const { setIsProfileMenuOpen, setIsLanguageMenuOpen } = useMenu();
   const { t } = useTranslation("button")
+
+  const closeMenu = () => {
+    setIsProfileMenuOpen(false);
+    setIsLanguageMenuOpen(false);
+  }
 
   return (
     <div>
       {isLoggedIn ? (
-        <button className="w-30 text-xl hover:cursor-pointer" onClick={openLogout}>
+        <button className="w-30 text-xl hover:cursor-pointer" 
+        onClick={() => {
+          closeMenu();
+          openLogout();
+        }}>
           {t("logout")}
         </button>
       ) : (
-        <button className="w-30 text-xl hover:cursor-pointer" onClick={openLogin}>
+        <button className="w-30 text-xl hover:cursor-pointer" 
+        onClick={() => {
+          closeMenu();
+          openLogin();
+        }}>
           {t("login")}
         </button>
       )}
