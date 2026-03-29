09d859794b2ddb3da7425f9a3bc8c270c584c1ac
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-03-29 17:08:40 +0300

    add PaginationPrevious.jsx as new button component, add rtl support

---
+++ frontend/src/components/buttons/PaginationPrevious.jsx
@@ -0,0 +1,29 @@
+import { useLayoutDirection } from '../../contexts/LayoutDirectionContext.jsx';
+import ChevronLeft from '@heroicons/react/24/solid/ChevronLeftIcon';
+import ChevronRight from '@heroicons/react/24/solid/ChevronRightIcon';
+
+export default function PaginationPrevious({
+  setCurrentPage,
+  scrollToSearchResultsHeader,
+}) {
+  const { isRTL } = useLayoutDirection();
+
+  return (
+    <button
+      className=":bg-actionButton p-1.5 rounded-md cursor-pointer hover:bg-actionButtonHover"
+      key="previous-page"
+      data-testid="previous-page"
+      onClick={(e) => {
+        e.preventDefault();
+        setCurrentPage((previous) => Math.max(1, previous - 1));
+        scrollToSearchResultsHeader();
+      }}
+    >
+      {isRTL ? (
+        <ChevronRight className="h-full size-5" />
+      ) : (
+        <ChevronLeft className="h-full size-5" />
+      )}
+    </button>
+  );
+}
