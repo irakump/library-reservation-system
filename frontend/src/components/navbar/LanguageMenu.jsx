be7040bbf7d9293941eb532d599362757965eafc
annimala <minerva.alanen@gmail.com>
2026-03-29 13:39:39 +0300

    language menu changes language

--- frontend/src/components/navbar/LanguageMenu.jsx
+++ frontend/src/components/navbar/LanguageMenu.jsx
@@ -3,24 +3,29 @@ import { useTranslation } from "react-i18next";
 import { useAuth } from "../../contexts/AuthContext";
 
 const LanguageMenu = () => {
-  const { t } = useTranslation(["navigation"]);
+  const { t, i18n } = useTranslation(["navigation"]);
   const { isLoggedIn } = useAuth();
+
+    const lngs = {
+        en: {nativeName: 'English'},
+        ja: {nativeName: '日本語'},
+        ar: {nativeName: 'العربية'},
+    };
 
   return (
     <div className={`absolute max-sm:w-full ${isLoggedIn ? "ltr:sm:right-75 rtl:sm:left-78" : "ltr:sm:right-30 rtl:sm:left-33"} text-center z-50`}>
       <ul className="flex flex-col w-full sm:w-40 bg-navbar border border-t-0 *:border-t *:p-2 text-xl font-bold *:hover:cursor-pointer">
-
-        <button>
-            <li>{t("footer.language.english")}</li>
-        </button>
-        
-        <button>
-            <li>{t("footer.language.arabic")}</li>
-        </button>
-
-        <button>
-            <li>{t("footer.language.japanese")}</li>
-        </button>
+          {Object.keys(lngs).map((lng) => (
+              <button
+                  type="submit"
+                  key={lng}
+                  onClick={() => i18n.changeLanguage(lng)}
+                  disabled={i18n.resolvedLanguage === lng}
+              >
+                  <li>{lngs[lng].nativeName}</li>
+              </button>
+          ))
+          }
         
       </ul>
     </div>
