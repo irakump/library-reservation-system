34cd667b8dec68c29630d30212f136ea589af0ef
irakump <ira.kumpula@outlook.com>
2026-03-29 12:16:33 +0300

    Fix book card text and image locations in RTL layout

--- frontend/src/components/Books/BookCard.jsx
+++ frontend/src/components/Books/BookCard.jsx
@@ -24,7 +24,7 @@ const BookCard = ({ book, pageType, setOpen, addToLoans }) => {
       className="bg-white rounded-lg p-2 gap-6 shadow hover:shadow-lg hover:opacity-90 hover:scale-[1.02] hover:cursor-pointer transition-all flex max-[350px]:flex-col min-w-[120px]"
       onClick={() => setOpen(book)}
     >
-      <div className="w-24 max-w-full h-auto mt-1.5 ml-1.5 shrink-0 pr-2 sm:pr-0">
+      <div className="w-24 max-w-full h-auto mt-1.5 ltr:ml-1.5 shrink-0 pr-2 rtl:mr-1.5 sm:pr-0">
         <img
           src={`/books/${pageType === "reservation" ? book.bookIsbn : book.isbn}.jpg`}
           alt={`Book image for ${book.title}`}
@@ -41,13 +41,13 @@ const BookCard = ({ book, pageType, setOpen, addToLoans }) => {
           </div>
         </div>
 
-        <p className="text-sm mb-1 text-left">
+        <p className="text-sm mb-1 text-left rtl:text-right">
           {book.authors
             ?.map((author) => `${author.firstName} ${author.lastName}`)
             .join(", ")}
         </p>
-        <p className="text-sm mb-1 text-left ">{book.year}</p>
-        <p className="text-sm mb-1 text-left capitalize">{book.genre}</p>
+        <p className="text-sm mb-1 text-left rtl:text-right">{book.year}</p>
+        <p className="text-sm mb-1 text-left rtl:text-right capitalize">{book.genre}</p>
 
         {book.availability === false && user && isLoggedIn && (
           <p className="text-sm mb-1">{t("queue_length", { queue_length: queueLength ?? "..." })}</p>
