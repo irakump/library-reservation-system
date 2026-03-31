fbad709b8fbe74189938edaedb79fec97387f909
irakump <ira.kumpula@outlook.com>
2026-03-31 15:32:50 +0300

    Add Japanese genres

--- backend/database/02_genre.sql
+++ backend/database/02_genre.sql
@@ -1,9 +1,9 @@
-INSERT INTO genre
+INSERT INTO genre (genre, genre_ja/*, genre_ar */) -- TODO: add arabic translations and after it, remove comments from genre_ar
 VALUES
-('children'),
-('nonfiction'),
-('fantasy'),
-('history'),
-('biography'),
-('science fiction'),
-('crime fiction');
+('children', '子供たち'),
+('nonfiction', 'ノンフィクション'),
+('fantasy', 'ファンタジー'),
+('history', '歴史'),
+('biography', 'バイオグラフィー'),
+('science fiction', 'サイエンスフィクション'),
+('crime fiction', '犯罪小説');
