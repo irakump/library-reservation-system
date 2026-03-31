7459f9789c33312db072937b9d6e959809c2c167
nurhana <nur.hana@metropolia.fi>
2026-03-31 19:54:10 +0300

    Add Arabic genres

--- backend/database/02_genre.sql
+++ backend/database/02_genre.sql
@@ -1,9 +1,9 @@
-INSERT INTO genre (genre, genre_ja/*, genre_ar */) -- TODO: add arabic translations and after it, remove comments from genre_ar
+INSERT INTO genre (genre, genre_ja, genre_ar)
 VALUES
-('children', '子供たち'),
-('nonfiction', 'ノンフィクション'),
-('fantasy', 'ファンタジー'),
-('history', '歴史'),
-('biography', 'バイオグラフィー'),
-('science fiction', 'サイエンスフィクション'),
-('crime fiction', '犯罪小説');
+('children', '子供たち', 'أطفال'),
+('nonfiction', 'ノンフィクション', 'غير خيالي'),
+('fantasy', 'ファンタジー', 'فانتازيا'),
+('history', '歴史', 'تاريخ'),
+('biography', 'バイオグラフィー', 'سيرة ذاتية'),
+('science fiction', 'サイエンスフィクション', 'خيال علمي'),
+('crime fiction', '犯罪小説', 'جريمة');
