1c059a670970dd2480127ecfb23538bdd1c6b3c5
irakump <ira.kumpula@outlook.com>
2026-04-11 09:20:02 +0300

    Cleanup webconfig and add Javadoc comment

--- backend/src/main/java/com/library/backend/WebConfig.java
+++ backend/src/main/java/com/library/backend/WebConfig.java
@@ -7,10 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * Configuration for CORS mappings.
+     */
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173",
+                        "http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
