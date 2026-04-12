c061289077aac641af8473a68a0c6b5a0c8d8202
nurhana <nur.hana@metropolia.fi>
2026-04-12 10:55:10 +0300

    Fix PMD issue in AuthorRepository.java

--- backend/src/main/java/com/library/backend/author/AuthorRepository.java
+++ backend/src/main/java/com/library/backend/author/AuthorRepository.java
@@ -2,5 +2,8 @@ package com.library.backend.author;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for Author data.
+ */
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 }
