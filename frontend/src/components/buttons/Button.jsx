4baca37141b404f376d27ce1e1800a039ab57677
nurhana <nur.hana@metropolia.fi>
2026-04-12 21:26:38 +0300

    Add propTypes validation in button

--- frontend/src/components/buttons/Button.jsx
+++ frontend/src/components/buttons/Button.jsx
@@ -1,14 +1,21 @@
+import PropTypes from "prop-types";
 function Button({ onClick, children }) {
   return (
-    <button className="bg-actionButton font-semibold rounded-xl px-6 py-2 hover:bg-actionButtonHover float-right cursor-pointer"
-    onClick={e => {
-      e.stopPropagation();
-      onClick();
-
-    }}>
+    <button
+      className="bg-actionButton font-semibold rounded-xl px-6 py-2 hover:bg-actionButtonHover float-right cursor-pointer"
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
       {children}
     </button>
   );
 }
+
+Button.PropTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 export default Button;
