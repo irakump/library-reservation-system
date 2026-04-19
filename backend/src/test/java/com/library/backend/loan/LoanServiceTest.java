fcd75773335ee41827658c42f61fbf456cec8993
annimala <minerva.alanen@gmail.com>
2026-04-19 19:37:16 +0300

    Added some and modified existing unit tests for LoanService class

--- backend/src/test/java/com/library/backend/loan/LoanServiceTest.java
+++ backend/src/test/java/com/library/backend/loan/LoanServiceTest.java
@@ -5,6 +5,8 @@ import com.library.backend.genre.Genre;
 import com.library.backend.genre.GenreRepository;
 import com.library.backend.language.Language;
 import com.library.backend.language.LanguageRepository;
+import com.library.backend.notifications.NotificationService;
+import com.library.backend.reservation.ReservationService;
 import com.library.backend.user.User;
 import com.library.backend.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +21,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -40,61 +43,76 @@ class LoanServiceTest {
     @Mock
     private LanguageRepository languageRepo;
 
+    @Mock
+    private ReservationService reservationService;
+
+    @Mock
+    private NotificationService notificationService;
+
     @InjectMocks
     private LoanService loanService;
 
     private User user;
     private Book book;
     private Loan loan;
+    private Loan returnedLoan;
+    private Loan upcomingLoan;
     private Genre genre;
     private Language language;
+    private LocalDate advancedDate;
 
     @BeforeEach
     void setUp() {
         user = new User("joku@gmail.com", "joku", "LKJKJS9987dakkcvdQl");
 
-        book = new Book("88281201228", "joku", "joku in japan", "joku", 2005, "jshdahdhad", "jsjdd", "shddd", "children", "finnish", true);
-        book.setAvailable(true);
+        book = new Book("88281201228", "joku", "joku in japan", "joku", 2005, "jshdahdhad", "jsjdd", "shddd", "children", "finnish", false);
+
+        Book returnedBook = new Book("11111", "Returned", "Japanese title", "Arabese title", 2833, "something", "something", "hsdksd", "skdks", "finnish", false );
 
         loan = new Loan(LocalDate.now().plusWeeks(2), user, book);
+
+        advancedDate = LocalDate.now().plusDays(2);
+        returnedLoan = new Loan(LocalDate.now().minusDays(3), user, returnedBook);
+        returnedLoan.setReturnDate(LocalDateTime.now().minusDays(10));
+
+        upcomingLoan = new Loan(advancedDate, user, book);
 
         genre = new Genre("children", "子供たち", "أطفال");
         language = new Language("finnish", "フィンランド語", "الفنلندية");
     }
 
 
+    //getAllLoans
     @Test
     void getAllLoans() {
-        when(loanRepo.findAll()).thenReturn(List.of(loan));
+        when(loanRepo.findAll()).thenReturn(List.of(loan, returnedLoan, upcomingLoan));
 
         List<LoanDTO> result = loanService.getAllLoans();
 
-        assertEquals(1, result.size());
+        assertEquals(3, result.size());
         verify(loanRepo, times(1)).findAll();
     }
 
     //getLoansByUser()
     @Test
     void getActiveLoansByUser() {
-        when(loanRepo.findByUserUserId(1)).thenReturn(List.of(loan));
+        when(loanRepo.findByUserUserId(1)).thenReturn(List.of(loan, returnedLoan, upcomingLoan));
 
         List<Loan> result = loanService.getActiveLoansByUser(1);
 
-        assertEquals(1, result.size());
-        verify(loanRepo).findByUserUserId(1);
+        assertThat(result).containsExactly(loan, upcomingLoan);
     }
 
 
     //getLoanHistory()
     @Test
     void testLoanHistoryByUser() {
-        loan.setReturnDate(LocalDateTime.now());
-        when(loanRepo.findByUserUserId(1)).thenReturn(List.of(loan));
+        upcomingLoan.setReturnDate(LocalDateTime.now());
+        when(loanRepo.findByUserUserId(1)).thenReturn(List.of(loan, upcomingLoan, returnedLoan));
 
         List<Loan> result = loanService.getLoanHistoryByUser(1);
 
-        assertEquals(1, result.size());
-        verify(loanRepo).findByUserUserId(1);
+        assertThat(result).containsExactly(upcomingLoan, returnedLoan);
     }
 
     @Test
@@ -133,10 +151,10 @@ class LoanServiceTest {
         List<LoanDTO> result = loanService.localizeLoans(List.of(loan), "ar-u-nu-arab");
 
         assertEquals(1, result.size());
-        assertEquals("joku", result.get(0).getTitle());
-        assertEquals("shddd", result.get(0).getDescription());
-        assertEquals("أطفال", result.get(0).getGenre());
-        assertEquals("الفنلندية", result.get(0).getLanguage());
+        assertEquals("joku", result.getFirst().getTitle());
+        assertEquals("shddd", result.getFirst().getDescription());
+        assertEquals("أطفال", result.getFirst().getGenre());
+        assertEquals("الفنلندية", result.getFirst().getLanguage());
     }
 
 
@@ -169,6 +187,23 @@ class LoanServiceTest {
     }
 
 
+    //returnLoan
+
+    @Test
+    void testReturnLoan() {
+        ReturnLoanDTO dto = new ReturnLoanDTO(10, 1, "88281201228");
+        when(bookRepo.findById("88281201228")).thenReturn(Optional.of(book));
+        when(loanRepo.findById(10)).thenReturn(Optional.of(loan));
+
+        loanService.returnLoan(dto);
+
+        assertThat(loan.getReturnDate()).isNotNull();
+        verify(loanRepo).save(loan);
+        verify(bookRepo).save(book);
+        verify(reservationService).processReservationQueue(book, loan);
+        verify(notificationService).notifyDueDate(user, book);
+    }
+
     @Test
     void testReturnNotFound() {
         ReturnLoanDTO dto = new ReturnLoanDTO(30, 1,"123");
@@ -178,5 +213,33 @@ class LoanServiceTest {
                 assertThrows(RuntimeException.class, () -> loanService.returnLoan(dto));
         assertEquals("loan not found", exception.getMessage());
 
+    }
+
+    //returnAllBooksByDueDate
+
+    @Test
+    void testProcessAllDueLoans() {
+        Loan loan1 = new Loan(LocalDate.now(), user, book);
+        Loan loan2 = new Loan(LocalDate.now(), user, book);
+        when(loanRepo.findByDueDate(LocalDate.now())).thenReturn(List.of(loan1, loan2));
+
+        loanService.returnAllBooksByDueDate();
+
+        assertThat(loan1.getReturnDate()).isNotNull();
+        assertThat(loan2.getReturnDate()).isNotNull();
+        verify(notificationService, times(2)).notifyDueDate(user, book);
+        verify(reservationService, times(2)).processReservationQueue(eq(book), any(Loan.class));
+    }
+
+
+    //shouldNotifyComingDueDate
+
+    @Test
+    void testNotifyForDueDateInTwoDays() {
+        when(loanRepo.findByDueDate(advancedDate)).thenReturn(List.of(upcomingLoan));
+
+        loanService.checkAllComingDueDate();
+
+        verify(notificationService).notifyComingUpDueDate(user, book);
     }
 }
