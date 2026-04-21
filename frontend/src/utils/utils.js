6b4554dce63e8cfaa23012acf676fec2720cea85
annimala <minerva.alanen@gmail.com>
2026-04-21 11:16:53 +0300

    Added input validation to localizeYear function

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 
 export function localizeYear(year) {
+    if (!year) return null;
     const date = new Date(year, 0, 1)
     return new Intl.DateTimeFormat(i18n.language, {year: 'numeric'}).format(date)
 }
